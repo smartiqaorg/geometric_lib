7dbec6d7ba9389758ec8dde52d8f1c73f218ede7
Daniil.K <dlkay@yandex.ru>
2021-03-30 14:55:01 +0300

    L-04: Tirangle.py fixed
    An error was in triangle's area function. It was successfully fixed

--- triangle.py
+++ triangle.py
@@ -1,6 +1,9 @@
+from math import sqrt
+
 def area(a, b, c):
-    return (a + b + c) / 2
+	p = (a + b + c) / 2
+	return sqrt(p*(p-a)*(p-b)*(p-c))
 
 
 def perimeter(a, b, c):
-    return a + b + c
+	return a + b + c
