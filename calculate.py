e35bedad3dab2f256d4abf8ae9c15e9fd60ca93e
Daniil.K <dlkay@yandex.ru>
2021-03-30 15:50:42 +0300

    L-04: Calculate.py added
    Calculate.py main designition is to compute area and/or perimeter
    of the given figure
    
    Now works only for rectangle. More figures will be added lately.

---
+++ calculate.py
@@ -0,0 +1,32 @@
+import rectangle
+
+
+
+figs = ['rectangle']
+funcs = ['perimeter', 'area']
+
+def calc(fig, func, size):
+	assert fig in figs
+	assert func in funcs
+
+	result = eval(f'{fig}.{func}(*{size})')
+	print(f'{func} of {fig} is {result}')
+
+if __name__ == "__main__":
+	func = ''
+	fig = ''
+	size = list()
+    
+	while fig not in figs:
+		fig = input(f"Enter figure name, avaliable are {figs}:\n")
+	
+	while func not in funcs:
+		func = input(f"Enter function name, avaliable are {funcs}:\n")
+	
+	while len(size) != sizes.get(f"{func}-{fig}", 1):
+		size = list(map(int, input("Input figure sizes separated by space, 2 for rectangle\n").split(' ')))
+
+	calc(fig, func, size)
+
+
+
