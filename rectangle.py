922ab32fa949fc694cf86fb1448b136ddcc05bad
asgard1999 <373417@niuitmo.ru>
2023-09-17 23:46:46 +0300

    была исправлен ошибка

--- rectangle.py
+++ rectangle.py
@@ -1,9 +1,6 @@
-Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def area(a, b): 
-...     return a * b 
-... 
-... def perimeter(a, b): 
-...     return a + b 
-... 
-SyntaxError: invalid syntax
+def area(a, b):
+    return a * b
+
+
+def perimeter(a, b):
+    return a + b
---
+++ triangle.py
@@ -0,0 +1,6 @@
+Python 3.10.11 (tags/v3.10.11:7d4cc5a, Apr  5 2023, 00:38:17) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def area(a, h): 
+...     return a * h / 2 
+... 
+... def perimeter(a, b, c): 
