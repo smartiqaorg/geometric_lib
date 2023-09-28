4c8bbe09edf2f566d5cec64c729d222045d0e342
Noloquideus <daniilmanukian@gmail.com>
2023-09-28 04:18:31 +0300

    docs: add description circle.py chore: code hasn't changed

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+'''принимает радиус круга (r), возвращает площадь круга'''
     return math.pi * r * r
 
 
 def perimeter(r):
+'''принимает радиус круга (r), длину окружности'''
     return 2 * math.pi * r
 
