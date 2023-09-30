6d117bac5c7538fb9971421d0a4d6c31685ebfd5
tamerland05 <85281349+tamerland05@users.noreply.github.com>
2023-09-30 07:40:23 +0300

    Put docs in triangle.py

--- triangle.py
+++ triangle.py
@@ -1,5 +1,7 @@
 def area(a, h):
+	'''Get side of triange and height lowered onto her and return area'''
 	return a * h / 2
 
 def perimeter(a, b, c):
+	'''Get sides of triangle and return his perimeter''
 	return a + b + c
