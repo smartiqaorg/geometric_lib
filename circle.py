8634a81a11a18627b10986933b2f230bf0296675
tamerland05 <85281349+tamerland05@users.noreply.github.com>
2023-09-30 07:34:41 +0300

    Put docs in circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Get a Radius of cercle and return his area'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Get a Radius of cercle and return his perimeter'''
     return 2 * math.pi * r
 
