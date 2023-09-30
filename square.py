a9b31b8b303e156b9b9c8bb6c7ab52e97f9bf5f9
tamerland05 <85281349+tamerland05@users.noreply.github.com>
2023-09-30 16:18:57 +0300

    Update docs in square.py

--- square.py
+++ square.py
@@ -1,9 +1,16 @@
 
 def area(a):
-    '''Get side of square and return his area'''
+    """
+    Get side of square and return his area
+ 	--------------------------------------
+    example of call: area(7) = 49
+    """
     return a * a
 
 
 def perimeter(a):
-    '''Get side of square and return his perimeter'''
+    """Get side of square and return his perimeter
+ 	----------------------------------------------
+    example of call: perimeter(7) = 28
+    """
     return 4 * a
