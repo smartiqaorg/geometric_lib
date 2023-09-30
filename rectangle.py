58e87205803f4b576158da386563b53312a6a7db
tamerland05 <85281349+tamerland05@users.noreply.github.com>
2023-09-30 07:36:06 +0300

    Put docs in rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,7 @@
 def area(a, b):
+	'''Get heiaght: a and weight: b of rectangle and return his area'''
 	return a * b
 
 def perimeter(a, b):
+	'''Get heiaght: a and weight: b of rectangle and return his perimeter'''
 	return 2 * (a + b)
