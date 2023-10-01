4dc69f1a678269b6d868dcf7ab44b0fcb58da307
alinazh <siberiangodness@mail.ru>
2023-10-01 16:56:25 +0300

    Added documentation to triangle.py and circle.py.

--- circle.py
+++ circle.py
@@ -2,9 +2,19 @@ import math
 
 
 def area(r):
+    '''
+    Returns the area of a circle.
+    :param r: radius - integer decimal number
+    :return: integer decimal number
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+    Returns the perimeter of a circle.
+    :param r: radius - integer decimal number
+    :return: integer decimal number
+    '''
     return 2 * math.pi * r
 
