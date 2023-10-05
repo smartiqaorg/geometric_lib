90a9e39f301390637a41d9447f4675ce358782b2
IlnurVildanov <vilnur758@gmail.com>
2023-10-05 11:30:34 +0300

    documentation added

--- circle.py
+++ circle.py
@@ -1,10 +1,10 @@
 import math
 
-
 def area(r):
+    ''' Принимает число r, возвращает площадь круга '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает число r, возвращает периметр круга '''
     return 2 * math.pi * r
-
