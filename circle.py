3d45a60cc3101dcee56588d8827b91e95926c26d
Vladimir_Barinov <reybrally@mail.ru.>
2023-10-07 21:55:48 +0300

    circle.py changed

--- circle.py
+++ circle.py
@@ -3,8 +3,10 @@ import math
 
 def area(r):
     return math.pi * r * r
+    '''принимает число r и возвращает площадь окружности с радиусом r'''
 
 
 def perimeter(r):
     return 2 * math.pi * r
+    '''принимает число r и возвращает периметр окружности с радиусом r'''
 
