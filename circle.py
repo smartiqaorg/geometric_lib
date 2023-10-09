f446c1c1f98b452ba464483810f8ff9f2111721c
Artem <me.peremy@gmail.com>
2023-10-10 00:06:35 +0300

    docs: new comments for circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимает число r, возвращает квадртат числа r умноженное на Pi'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает число r, возвращает удвоенное произведение Pi на r'''
     return 2 * math.pi * r
 
