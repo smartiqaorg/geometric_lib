3a9de8519f0c9f898b4a015189c17111d4a7efbd
MaximR <200011kkr@gmail.com>
2023-10-09 18:34:59 +0300

    Добавленно описание к файлу circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимает число r, возвращает число r в квадрате, умноженное на пи'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    'Принимает число r, возвращает число r умноженное на 2 и на пи'
     return 2 * math.pi * r
 
