9093c8fbc8eda5f2c61d0991055a875d11a1371a
ISLAM <islam_omarov_2018@mail.ru>
2023-10-09 17:27:01 +0300

    modifie: rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,18 @@
-def area(a, b): 
-    return a * b 
-
-def perimeter(a, b): 
-    return 2 * (a + b) 
+def shift(lst, steps):
+    if steps < 0:
+        steps = abs(steps)
+        for i in range(steps):
+            lst.append(lst.pop(0))
+    else:
+        for i in range(steps):
+            lst.insert(0, lst.pop())
+ 
+ 
+nums = [4, 5, 6, 7, 8, 9, 0]
+print(nums)
+ 
+shift(nums, -2)
+print(nums)
+ 
+shift(nums, 3)
+print(nums)
