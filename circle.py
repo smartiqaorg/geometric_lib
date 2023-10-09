ff1acf8ba3bf17428c1bbfca35cc60058e19e416
ISLAM <islam_omarov_2018@mail.ru>
2023-10-09 18:26:15 +0300

    add comment in circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимает число r, возвращает квадрат r помноженный на math.pi'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает число r, возвращает r помноженное на math.pi помноженное на 2'''
     return 2 * math.pi * r
 
