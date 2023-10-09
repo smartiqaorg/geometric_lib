88e5288575bc4cc455384ae6b12dbe3c51309672
ISLAM <islam_omarov_2018@mail.ru>
2023-10-09 18:25:40 +0300

    add comment in rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,18 +1,7 @@
-def shift(lst, steps):
-    if steps < 0:
-        steps = abs(steps)
-        for i in range(steps):
-            lst.append(lst.pop(0))
-    else:
-        for i in range(steps):
-            lst.insert(0, lst.pop())
- 
- 
-nums = [4, 5, 6, 7, 8, 9, 0]
-print(nums)
- 
-shift(nums, -2)
-print(nums)
- 
-shift(nums, 3)
-print(nums)
+def area(a, b):
+    '''Принимает числа a и b, возвращает их произведение'''
+    return a * b
+
+def perimeter(a, b):
+    '''Принимает числа a и b, возвращает их сумму, умноженную на 2'''
+    return (a + b)*2
