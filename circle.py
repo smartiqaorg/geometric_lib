90cb1043a93b2e00fed242289f94dfeccda5d128
t1xelL <sidorov_timofey05@mail.ru>
2023-10-10 21:48:09 +0300

    add description in rectangle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    
     return math.pi * r * r
 
 
 def perimeter(r):
+   
     return 2 * math.pi * r
 
--- square.py
+++ square.py
@@ -1,7 +1,9 @@
 
 def area(a):
+    
     return a * a
 
 
 def perimeter(a):
+    
     return 4 * a
