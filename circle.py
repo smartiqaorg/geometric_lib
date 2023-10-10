2c56b0a44b428cbebe59265f13bc9999b75d80d6
arsllnva <144905493+arsllnva@users.noreply.github.com>
2023-10-10 19:23:29 +0300

    изменение

--- circle.py
+++ circle.py
@@ -1,10 +1,12 @@
 import math
-
-
 def area(r):
+    '''Принимает число r,возвращает площадь круга с радиусом r
+    К примеру,для r=5 print(area(5)) выведет 78,53982
+    '''
     return math.pi * r * r
-
-
 def perimeter(r):
+    '''Принимает число r,возвращает длину окружности
+    Для r=2 print(perimeter(2)) выведет 12,56637
+    '''
     return 2 * math.pi * r
 
