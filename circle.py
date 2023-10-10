bd2cc6b61e9e762cee33cb80097830262a161b98
Kirill <mironovkirill259@gmail.com>
2023-10-10 23:41:24 +0300

    Задекларировал файлы: square.py rectangle.py circle.py triangle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    """Принемает сторону и выводит площадь фигуры"""
     return math.pi * r * r
 
 
 def perimeter(r):
+    """Принемает сторону и выводит периметр фигуры"""
     return 2 * math.pi * r
 
