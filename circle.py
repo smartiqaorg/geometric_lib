14fe169d0873286f30eed02c4716787f120a9135
KostyaZap <dddf1n@yandex.ru>
2023-10-11 18:22:46 +0300

    docs: added docs in circle.py, rectangle.py, square.py, triangle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    ''' Принимает r - радиус, возвращает площадь круга '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    ''' Принимает r - радиус, возвращает длину круга '''
     return 2 * math.pi * r
 
