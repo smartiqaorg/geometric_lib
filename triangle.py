e74ae8d2770337b38c1706f4ba8baf593b10749e
Ksenia Ivanova <sweetbananyauwu@gmail.com>
2023-10-12 12:33:55 +0300

    docs: added comments for square.py and triangle.py

--- triangle.py
+++ triangle.py
@@ -1,5 +1,5 @@
-def area(a, h): 
-    return a * h / 2 
+def area(a, h): # function that returns area of triangle
+    return a * h / 2  # gets two varibles and return one
 
-def perimeter(a, b, c): 
-    return a + b + c 
+def perimeter(a, b, c): # function that returns perimeter of square 
+    return a + b + c # gets two varibles and return one
