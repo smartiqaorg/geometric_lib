278f62a50c1b0712ae15c571909d42c6cc2fbe04
Ksenia Ivanova <sweetbananyauwu@gmail.com>
2023-10-12 12:31:12 +0300

    docs: added comments for circle.py and rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,5 @@
-def area(a, b): 
+def area(a, b): # Function that returns area of rectangle
     return a * b 
 
-def perimeter(a, b): 
-    return 2*(a + b) 
+def perimeter(a, b): # function that returns perimeter of rectangle
+    return 2*(a + b) # gets 2 varibles and returns one
