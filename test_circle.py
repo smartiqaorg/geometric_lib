14cbdfbe45ad400a606ff93436ebb20263ca35c0
Polyanskiy Egor Olegovich <142446788+upravlenets@users.noreply.github.com>
2023-10-28 01:47:38 +0300

    the test has been added

--- test_circle.py
+++ test_circle.py
@@ -4,16 +4,16 @@ from circle import perimeter
 class RectangleTestCase(unittest.TestCase):
     def test_zero_r(self):
         resarea = area(0)
-        resperimetr = area(0)
+        resperimetr = perimeter(0)
         self.assertEqual(resarea, 0)
         self.assertEqual(resperimetr, 0)
     def test_single_к(self):
         resarea = area(1)
-        resperimetr = area(1)
+        resperimetr = perimeter(1)
         self.assertEqual(resarea, 3.141592653589793)
-        self.assertEqual(resperimetr, 3.141592653589793)
+        self.assertEqual(resperimetr, 6.283185307179586)
     def test_examplefromdeclaration_к(self):
         resarea = area(4)
-        resperimetr = area(4)
+        resperimetr = perimeter(4)
         self.assertEqual(resarea, 50.26548245743669)
-        self.assertEqual(resperimetr, 50.26548245743669)
+        self.assertEqual(resperimetr, 25.132741228718345)
