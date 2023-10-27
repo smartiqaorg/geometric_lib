9688bb216644c89d81ea8b2ed512df1457c160ea
Polyanskiy Egor Olegovich <142446788+upravlenets@users.noreply.github.com>
2023-10-28 01:54:13 +0300

    the test has been added

--- test_rectangle.py
+++ test_rectangle.py
@@ -1,6 +1,6 @@
 import unittest
-from triangle import area
-from triangle import perimeter
+from rectangle import area
+from rectangle import perimeter
 class RectangleTestCase(unittest.TestCase):
     def test_zero_ab(self):
         resarea = area(0,0)
@@ -11,7 +11,7 @@ class RectangleTestCase(unittest.TestCase):
         resarea = area(1,1)
         resperimetr = perimeter(1,1)
         self.assertEqual(resarea, 1)
-        self.assertEqual(resperimetr, 1)
+        self.assertEqual(resperimetr, 4)
     def test_examplefromdeclaration_ab(self):
         resarea = area(4,4)
         resperimetr = perimeter(4,4)
