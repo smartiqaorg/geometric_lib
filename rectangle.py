07e7a61f17c63b54ad24a5372159561adf5862b2
Polyanskiy Egor Olegovich <142446788+upravlenets@users.noreply.github.com>
2023-10-28 01:30:43 +0300

    code was fixed

--- rectangle.py
+++ rectangle.py
@@ -1,25 +1,15 @@
-import unittest
-…
-class RectangleTestCase(unittest.TestCase):
-   def test_zero_mul(self):
-       res = area(10, 0)
-       self.assertEqual(res, 0)
-       
-   def test_square_mul(self):
-       res = area(10, 10)
-       self.assertEqual(res, 100)
-
-def area(a,b):
+def area(a, b):
+    '''
+    Принимает числа а и b - стороны прямоугольника
+    Возвращает площадь прямоугольника
+    Пример: a=4, b=4, area(4,4)=16
+    '''
     return a * b
-'''
-Принимает числа а и b - стороны прямоугольника
-Возвращает площадь прямоугольника
-Пример: a=4, b=4, area(4,4)=16
-'''
-def perimeter(a,b):
-'''
-Принимает числа а и b - стороны прямоугольника
-Возвращает периметр прямоугольника
-Пример: a=4, b=4, perimeter(4,4)=8
-'''
-    return (a+b)*2
+
+def perimeter(a, b):
+    '''
+    Принимает числа а и b - стороны прямоугольника
+    Возвращает периметр прямоугольника
+    Пример: a=4, b=4, perimeter(4,4)=16
+    '''
+    return 2 * (a + b)
