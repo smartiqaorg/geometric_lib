c10502c3a36db719ec80ea2fa4929d661c02d79c
Valerie <valeriekim.jj@gmail.com>
2023-11-01 02:10:44 +0300

    implemented tests for triangle.py functions

--- test_triangle.py
+++ test_triangle.py
@@ -1,10 +1,17 @@
 import unittest
+from triangle import area
+from triangle import perimeter
 
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
+class TriangleTestCase(unittest.TestCase):
+    def test_area(self):
+        res = area(5, 7)
+        self.assertEqual(res, 17.5)
+    def test_zero_area(self):
+        res = area(0, 5)
+        self.assertEqual(res, 0)
+    def test_perimeter(self):
+        res = perimeter(2, 3, 5)
+        self.assertEqual(res, 10)
+    def test_zero_perimeter(self):
+        res = perimeter(0, 3, 5)
+        self.assertEqual(res, 'incorrect input')
