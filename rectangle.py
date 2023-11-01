bbefd4f228fdc25f704b22619243967c0523c54f
Polyanskiy Egor Olegovich <142446788+upravlenets@users.noreply.github.com>
2023-11-01 10:26:01 +0300

    The test was added to program file

--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,21 @@
+import unittest
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_ab(self):
+        resarea = area(0,0)
+        resperimetr = perimeter(0,0)
+        self.assertEqual(resarea, 0)
+        self.assertEqual(resperimetr, 0)
+    def test_single_ab(self):
+        resarea = area(1,1)
+        resperimetr = perimeter(1,1)
+        self.assertEqual(resarea, 1)
+        self.assertEqual(resperimetr, 4)
+    def test_examplefromdeclaration_ab(self):
+        resarea = area(4,4)
+        resperimetr = perimeter(4,4)
+        self.assertEqual(resarea, 16)
+        self.assertEqual(resperimetr, 16)
+
 def area(a, b):
     '''
     Принимает числа а и b - стороны прямоугольника
--- triangle.py
+++ triangle.py
@@ -1,3 +1,20 @@
+import unittest
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_ab(self):
+        resarea = area(0,0)
+        resperimetr = perimeter(0,0,0)
+        self.assertEqual(resarea, 0)
+        self.assertEqual(resperimetr, 0)
+    def test_single_ab(self):
+        resarea = area(1,1)
+        resperimetr = perimeter(1,1,1)
+        self.assertEqual(resarea, 0.5)
+        self.assertEqual(resperimetr, 3)
+    def test_examplefromdeclaration_ab(self):
+        resarea = area(4,4)
+        resperimetr = perimeter(4,4,4)
+        self.assertEqual(resarea, 8)
+        self.assertEqual(resperimetr, 12)
 def area(a,h):
     '''
     Принимает числа а и h - сторона и высота треугольника
