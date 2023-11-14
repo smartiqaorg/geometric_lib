c2efffacbb0b14578c5e3c6a668575018ac37e69
zaimkin <Zaimkin.work@gmail.com>
2023-11-14 23:52:49 +0300

    unit-test added

--- circle.py
+++ circle.py
@@ -1,4 +1,5 @@
 import math
+import unittest
 
 
 def area(r):
@@ -25,4 +26,56 @@ def perimeter(r):
             произведение 2 на Число Пи на радиус (r), то есть длина окружности.
     '''
     return 2 * math.pi * r
+
+class CircleTestCase(unittest.TestCase):
+
+    def test_area_1(self):
+        res = area(1)
+        self.assertEqual(res, math.pi * (1**2), "area1_FAIL")
+
+    def test_area_2(self):
+        res = area(2)
+        self.assertEqual(res, math.pi * (2**2),"area2_FAIL")
+
+    def test_area_3(self):
+        res = area(3)
+        self.assertEqual(res, math.pi * (3 ** 2), "area3_FAIL")
+
+    def test_area_4(self):
+        res = area(4)
+        self.assertEqual(res, math.pi * (4 ** 2), "area4_FAIL")
+
+    def test_area_5(self):
+        res = area(5)
+        self.assertEqual(res, math.pi * (5 ** 2), "area5_FAIL")
+
+    def test_per_1(self):
+        res = perimeter(1)
+        self.assertEqual(res, 2 * math.pi * 1)
+
+    def test_per_2(self):
+        res = perimeter(2)
+        self.assertEqual(res, 2 * math.pi * 2)
+
+    def test_per_3(self):
+        res = perimeter(3)
+        self.assertEqual(res, 2 * math.pi * 3)
+
+    def test_per_4(self):
+        res = perimeter(4)
+        self.assertEqual(res, 2 * math.pi * 4)
+
+    def test_per_5(self):
+        res = perimeter(5)
+        self.assertEqual(res, 2 * math.pi * 5)
+
+
+
+
+
+
+
+
+
+
 
--- triangle.py
+++ triangle.py
@@ -1,3 +1,4 @@
+import unittest
 def area(a, h):
     '''
     возвращает площадь треугольника с длиной одной из сторон a, и длины высоты, которая к ней проведена, h.
@@ -22,4 +23,46 @@ def perimeter(a, b, c):
 
         Возвращаемое значение (число): сумма a, b, c.
     '''
-    return a + b + c 
+    return a + b + c
+
+class trianlgeTestCase(unittest.TestCase):
+
+    def test_per_1(self):
+        res = perimeter(2,2,3)
+        self.assertEqual(res, 2 + 2 + 3)
+
+    def test_per_2(self):
+        res = perimeter(4,5,6)
+        self.assertEqual(res, 4 + 5 + 6)
+
+    def test_per_3(self):
+        res = perimeter(7, 6, 5)
+        self.assertEqual(res, 5 + 6 + 7)
+
+    def test_per_4(self):
+        res = perimeter(4, 4, 4)
+        self.assertEqual(res, 12)
+
+    def test_per_5(self):
+        res = perimeter(10, 4, 4)
+        self.assertEqual(res, 18)
+
+    def test_area_1(self):
+        res = area(1, 4)
+        self.assertEqual(res, 2)
+
+    def test_area_2(self):
+        res = area(2, 4)
+        self.assertEqual(res, 4)
+
+    def test_area_3(self):
+        res = area(3, 4)
+        self.assertEqual(res, 6)
+
+    def test_area_4(self):
+        res = area(3, 5)
+        self.assertEqual(res, 7.5)
+
+    def test_area_5(self):
+        res = area(5, 5)
+        self.assertEqual(res, 12.5)
