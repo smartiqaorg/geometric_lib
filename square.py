27132517d14307b18e5f8c43bc87012249b0cac6
Valeriy Pryazhnikov <valerapryazhnikov@gmail.com>
2023-11-14 20:35:14 +0300

    add tests to square and triangle

--- square.py
+++ square.py
@@ -1,7 +1,24 @@
-
+import unittest
 def area(a):
     return a * a
 
 
 def perimeter(a):
     return 4 * a
+
+class SquareTestCase(unittest.TestCase):
+    def test_area_one(self):
+        res = area(1)
+        self.assertEqual(res, 1)
+
+    def test_area_5(self):
+        res = area(5)
+        self.assertEqual(res, 25)
+
+    def test_perimetr_one(self):
+        res = perimeter(1)
+        self.assertEqual(res, 4)
+
+    def test_perimetr_twenty(self):
+        res = perimeter(20)
+        self.assertEqual(res, 80)
