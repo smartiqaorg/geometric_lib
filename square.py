edb9f4f3e6fd024cc9d65b7b37be60f15f7bacb3
kashasnish <knniish@gmail.com>
2023-11-15 02:56:23 +0300

    добавлен unit test в square.py

--- square.py
+++ square.py
@@ -7,3 +7,23 @@ def area(a):
 def perimeter(a):
     ''' принимает значение а,а возвращает периметр квадрата со сторонами а'''
     return 4 * a
+
+
+import unittest
+
+
+class TestSquareFunctions(unittest.TestCase):
+    def test_area(self):
+        # Проверка правильности вычисления площади квадрата
+        side_length = 5
+        expected_area = side_length * side_length
+        result = area(side_length)
+        self.assertEqual(result, expected_area, f"Ожидаемая площадь: {expected_area}, Фактическая площадь: {result}")
+
+    def test_perimeter(self):
+        # Проверка правильности вычисления периметра квадрата
+        side_length = 5
+        expected_perimeter = 4 * side_length
+        result = perimeter(side_length)
+        self.assertEqual(result, expected_perimeter,f"Ожидаемый периметр: {expected_perimeter}, Фактический периметр: {result}")
+
