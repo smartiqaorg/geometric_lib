90ef3c3b761400a8744b4209c5c7d454b7438849
mirnih <milana.archakova.06@mail.ru>
2023-11-15 10:39:33 +0300

    added unittest in all files

--- circle.py
+++ circle.py
@@ -1,5 +1,5 @@
 import math
-
+import unittest
 
 def area(r):
     '''Принимает радиус r, возвращает площадь круга с радиусом r'''
@@ -10,3 +10,14 @@ def perimeter(r):
     '''Принимает радиус r, возвращает периметр круга с радиусом r'''
     return 2 * math.pi * r
 
+
+class CircleTestCase(unittest.TestCase):
+
+    def test_zero_area(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_zero_perim(self):
+
+        res = perimeter(0)
+        self.assertEqual(res, 0)
--- square.py
+++ square.py
@@ -6,3 +6,28 @@ def area(a):
 def perimeter(a):
     '''Принимает число a (сторона квадрата), возвращает периметр квадрата'''
     return 4 * a
+
+import unittest
+
+class SquareTestCase(unittest.TestCase):
+
+    def test_zero_area(self):
+
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_zero_perim(self):
+
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_square_area(self):
+
+        res = area(10)
+        self.assertEqual(res, 100)
+
+    def test_sqaure_perimeter(self):
+
+        res = perimeter(10)
+        self.assertEqual(res, 40)
+  
