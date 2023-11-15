51a67d533f492a6ccad1d08f7b0edbab63218031
Bushkov Nikita <408338@niuitmo.ru>
2023-11-15 22:45:56 +0300

    test: added tests for triangle

---
+++ triangle_test.py
@@ -0,0 +1,32 @@
+import unittest
+from triangle import area
+from triangle import perimeter
+
+class TriangleTestCase(unittest.TestCase):
+
+    def test_triangle_area_correct(self):
+        res = area(13, 24)
+        self.assertEqual(res, 156)
+
+    def test_triangle_area_wrong(self):
+        res = area(-23, 124)
+        self.assertEqual(res, "negative number cannot be used")
+
+    def test_triangle_area_null(self):
+        res = area(0, 2)
+        self.assertEqual(res, "null cannot be used")
+
+    def test_triangle_perimeter_correct(self):
+        res = perimeter(2, 4, 6)
+        self.assertEqual(res, 12)
+
+    def test_triangle_perimeter_wrong(self):
+        res = perimeter(12, -19, 23)
+        self.assertEqual(res, "negative number cannot be used")
+
+    def test_triangle_perimeter_null(self):
+        res = perimeter(0, 3 , 9)
+        self.assertEqual(res, "null cannot be used")
+        
+if __name__ == '__main__':
+    unittest.main()
