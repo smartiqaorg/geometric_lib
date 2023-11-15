f80a327dbbb8eb7627395f3223202441ea60fb54
Traffaello <Matvienko.mp@gmail.com>
2023-11-15 10:42:40 +0300

    Git_test added for rectangle

---
+++ git_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_mul(self):
+        res = area(10, 0)
+        self.assertEqual(res, 0)
+
+    def test_square_mul(self):
+        res = area(10, 10)
+        self.assertEqual(res, 100)
