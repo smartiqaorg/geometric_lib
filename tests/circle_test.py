e318840bc3fc999e128a15170aa35229b5d7a931
368520_taisiya <nikita200527221@gmail.com>
2023-11-15 16:06:35 +0300

    Tests have been updated

--- tests/circle_test.py
+++ tests/circle_test.py
@@ -13,8 +13,8 @@ class CircleTestCase(unittest.TestCase):
     def test_valid_radius(self):
         res_area = area(5)
         res_perimeter = perimeter(5)
-        self.assertEqual(res_area, 78.54)
-        self.assertEqual(res_perimeter, 31.42)
+        self.assertEqual(res_area, 78.53981633974483)
+        self.assertEqual(res_perimeter, 31.41592653589793)
 
 if __name__ == '__main__':
     unittest.main()
