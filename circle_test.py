d38677240c78b6605505bb9ea53a60fffa88ce71
ssandaa <oziniek.art@mail.ru>
2023-11-15 23:10:10 +0300

    test: added tests for circle

--- circle_test.py
+++ circle_test.py
@@ -8,7 +8,7 @@ class CircleTestCase(unittest.TestCase):
 
     def test_circle_area_neNorm(self):
         res = area(-5)
-        self.assertEqual(res, 'радиус не может быть отрицательным')
+        self.assertEqual(res, 'the radius cannot be negative')
 
     def test_circle_area_real(self):
         res = area(5.5)
@@ -24,7 +24,7 @@ class CircleTestCase(unittest.TestCase):
 
     def test_circle_perimeter_neNorm(self):
         res = perimeter(-5)
-        self.assertEqual(res, 'радиус не может быть отрицательным')
+        self.assertEqual(res, 'the radius cannot be negative')
 
     def test_circle_perimeter_real(self):
         res = perimeter(5.5)
