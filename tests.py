245eae8256e6ce37c941aacbf4fe73a6262ae8c7
daniil3006 <daniil.stank@gmail.com>
2023-11-15 13:09:42 +0300

    внесены изменения

--- tests.py
+++ tests.py
@@ -1,50 +1,61 @@
-def area(a, b):
+import math
+
+def area_rectangle(a, b):
     return a * b
-def perimeter(a, b):
+def perimeter_rectangle(a, b):
     return 2*(a + b)
+
+def area_square(a):
+    return a * a
+
+def perimeter_square(a):
+    return 4 * a
+
+def area_triangle(a, h):
+	return a * h / 2
+def perimeter_triangle(a, b, c):
+	return a + b + c
+
+def area_circle(r):
+    return math.pi * r * r
+def perimeter_circle(r):
+    return 2 * math.pi * r
 
 import unittest
 
-class RectangleTestCase(unittest.TestCase):
-    def test_one_area(self):
-        res = area(10, 0)
+class TestCase(unittest.TestCase):
+    def test_one_area_rectangle(self):
+        res = area_rectangle(10, 0)
         self.assertEqual(res, 0)
 
-    def test_two_area(self):
-        res = area(5, 25)
+    def test_two_perimetr_rectangle(self):
+        res = area_rectangle(5, 25)
         self.assertEqual(res, 125)
 
-    def test_three_area(self):
-        res = area(14, 196)
-        self.assertEqual(res, 2744)
+    def test_three_area_square(self):
+        res = area_square(14)
+        self.assertEqual(res, 196)
 
-    def test_four_area(self):
-        res = area(21, 21)
-        self.assertEqual(res, 441)
+    def test_four_perimetr_square(self):
+        res = perimeter_square(21)
+        self.assertEqual(res, 84)
 
-    def test_five_area(self):
-        res = area(7262, 9146)
-        self.assertEqual(res, 66418252)
+    def test_five_area_triangle(self):
+        res = area_triangle(7262, 9146)
+        self.assertEqual(res, 33209126)
 
-    def test_one_perimeter(self):
-        res = perimeter(10, 10)
+    def test_six_perimeter_triangle(self):
+        res = perimeter_triangle(10, 10, 20)
         self.assertEqual(res, 40)
 
-    def test_two_perimeter(self):
-        res = perimeter(5, 44)
-        self.assertEqual(res, 98)
+    def test_seven_area_circle(self):
+        res = area_circle(5)
+        self.assertEqual(res, 78.53981633974483)
 
-    def test_three_perimeter(self):
-        res = perimeter(267, 823)
-        self.assertEqual(res, 2180)
+    def test_eight_perimetr_circle(self):
+        res = perimeter_circle(5)
+        self.assertEqual(res, 31.41592653589793)
 
-    def test_four_perimeter(self):
-        res = perimeter(13, 422)
-        self.assertEqual(res, 870)
-
-    def test_five_perimeter(self):
-        res = perimeter(3874, 7263)
-        self.assertEqual(res, 22274)
 
 if __name__ == '__main__':
     unittest.main()
