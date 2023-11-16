2d7b06e493abae3e5e33b24a1e1828de8f381bce
parsival1999 <373417@niuitmo.ru>
2023-11-16 12:22:57 +0300

    Tests added

--- circle.py
+++ circle.py
@@ -1,5 +1,5 @@
 import math
-
+import unittest
 
 def area(r):
     return math.pi * r * r
@@ -8,3 +8,23 @@ def area(r):
 def perimeter(r):
     return 2 * math.pi * r
 
+def test_area():
+    assert area(1) == 3.141592653589793
+    assert area(133.41) == 55914.78264587542
+
+def test_perimeter():
+    assert perimeter(1) == 6.283185307179586
+    assert perimeter(133.41) == 838.2397518308286
+
+class TestCircle(unittest.TestCase):
+
+    def test_area(self):
+        self.assertEqual(area(1), 3.141592653589793)
+        self.assertEqual(area(133.41), 55914.78264587542)
+
+    def test_perimeter(self):
+        self.assertEqual(perimeter(1), 6.283185307179586)
+        self.assertEqual(perimeter(133.41), 838.2397518308286)
+
+if __name__ == '__main__':
+    unittest.main()
--- rectangle.py
+++ rectangle.py
@@ -1,6 +1,33 @@
+import unittest
+
 def area(a, b):
     return a * b
 
-
 def perimeter(a, b):
-    return (a + b)*2
+    return (a + b) * 2
+
+def test_area():
+    assert area(2, 3) == 6
+    assert area(4.6, 5.6) == 25.76
+
+def test_perimeter():
+    assert perimeter(2, 3) == 10
+    assert perimeter(4.6, 5.6) == 20.4
+
+if __name__ == '__main__':
+    test_area()
+    test_perimeter()
+
+class TestRectangle(unittest.TestCase):
+
+    def test_area(self):
+        self.assertEqual(area(2, 3), 6)
+        self.assertEqual(area(4, 5), 20)
+
+    def test_perimeter(self):
+        self.assertEqual(perimeter(2, 3), 10)
+        self.assertEqual(perimeter(4, 5), 18)
+
+if __name__ == '__main__':
+    unittest.main()
+
