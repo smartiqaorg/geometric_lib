9525458d8108f72466bfeb0c409b0cb94ee4f8d7
bruhndo <cthulhu709@gmail.com>
2023-11-21 18:36:28 +0300

    added descriprions in circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимает радиус круга (r), возвращает его площадь.'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает радиус круга (r), возвращает его периметр.'''
     return 2 * math.pi * r
 
