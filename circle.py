08b89e2499188ed2f77e7b004787426aa637d9c9
gront <408507@edu.itmo.ru>
2023-11-22 00:13:18 +0300

    docs: Add description for each function

--- circle.py
+++ circle.py
@@ -1,10 +1,27 @@
 import math
 
-
 def area(r):
+    '''
+    Принимает число r, значение r - радиус круга
+
+      Параметры:
+          r (int) или (float)
+
+      Возвращаемое значение:
+        Площадь круга (int) или (float)
+     '''
+
     return math.pi * r * r
 
 
 def perimeter(r):
-    return 2 * math.pi * r
+    '''
+        Принимает число r, значение r - радиус круга
 
+          Параметры:
+              r (int) или (float)
+
+          Возвращаемое значение:
+            Периметр круга (int) или (float)
+         '''
+    return 2 * math.pi * r
