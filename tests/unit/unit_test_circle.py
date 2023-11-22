a284c79bf9778b1e41dd48519e29320da65d0595
mashiklavashik <409162@niuitmo.ru>
2023-11-22 22:06:27 +0300

    New file: Added unit tests

---
+++ tests/unit/unit_test_circle.py
@@ -0,0 +1,18 @@
+import unittest
+from circle import area, perimeter
+
+
+class CircleTest(unittest.TestCase):
+    def test_area(self):
+        self.assertAlmostEqual(area(8), 201.06192982974676)
+        self.assertAlmostEqual(area(4), 50.26548245743669)
+        self.assertAlmostEqual(area(34), 3631.6811075498013)
+
+    def test_perimeter(self):
+        self.assertAlmostEqual(perimeter(4), 25.132741228718345)
+        self.assertAlmostEqual(perimeter(8), 50.26548245743669)
+        self.assertAlmostEqual(perimeter(15), 94.24777960769379)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/unit/unit_test_square.py
@@ -0,0 +1,18 @@
+import unittest
+from square import area, perimeter
+
+
+class SquareTest(unittest.TestCase):
+    def test_area(self):
+        self.assertAlmostEqual(area(5), 25)
+        self.assertAlmostEqual(area(3), 9)
+        self.assertAlmostEqual(area(6.2), 38.44)
+
+    def test_perimeter(self):
+        self.assertAlmostEqual(perimeter(3), 12)
+        self.assertAlmostEqual(perimeter(10), 40)
+        self.assertAlmostEqual(perimeter(5.5), 22)
+
+
+if __name__ == '__main__':
+    unittest.main()
