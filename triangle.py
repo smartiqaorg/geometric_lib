8a07b3888167a944121e76aa7d0297275072f0c0
AkihiroKano <graf.orlov2005@gmail.com>
2023-11-22 21:49:56 +0300

    changing a file triangle.py: adding unit tests

--- triangle.py
+++ triangle.py
@@ -1,5 +1,43 @@
-def area(a, h): 
-    return a * h / 2 
+import unittest
 
-def perimeter(a, b, c): 
-    return a + b + c 
+
+def area(a, h):
+    return a * h / 2
+
+
+def perimeter(a, b, c):
+    return a + b + c
+
+
+class RectangleTestCase(unittest.TestCase):
+    def test_square_mul(self):
+        res1 = area(10, 0)
+        self.assertEqual(res1, 0)
+
+        res2 = area(10, 1)
+        self.assertEqual(res2, 5)
+
+        res3 = area(5, 6)
+        self.assertEqual(res3, 15)
+
+        res4 = area(100, 100)
+        self.assertEqual(res4, 5000)
+
+        res5 = area(26, 25)
+        self.assertEqual(res5, 325)
+
+    def test_perimeter_mul(self):
+        res1 = perimeter(0, 0, 0)
+        self.assertEqual(res1, 0)
+
+        res2 = perimeter(10, 1, 5)
+        self.assertEqual(res2, 16)
+
+        res3 = perimeter(5, 5, 5)
+        self.assertEqual(res3, 15)
+
+        res4 = perimeter(100, 100, 200)
+        self.assertEqual(res4, 400)
+
+        res5 = perimeter(25, 25, -30)
+        self.assertEqual(res5, 20)
