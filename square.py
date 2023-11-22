95ca7699e2b091360dc0e44dc069ae0c29ec1acb
Vladimir_Barinov <reybrally@mail.ru.>
2023-11-22 15:21:53 +0300

    tests added in square.py

--- square.py
+++ square.py
@@ -1,3 +1,5 @@
+import unittest
+
 
 def area(a):
     return a * a
@@ -5,3 +7,34 @@ def area(a):
 
 def perimeter(a):
     return 4 * a
+
+
+class SquareTest(unittest.TestCase):
+
+    def test_area1(self):
+        res = area(7)
+        self.assertEqual(res, 49)
+
+    def test_area2(self):
+        res = area(17)
+        self.assertEqual(res, 289)
+
+    def test_area3(self):
+        res = area(104)
+        self.assertEqual(res, 10_816)
+
+    def test_perimeter1(self):
+        res = perimeter(4)
+        self.assertEqual(res, 16)
+
+    def test_peimeter2(self):
+        res = perimeter(17)
+        self.assertEqual(res, 68)
+
+    def test_perimeter3(self):
+        res = perimeter(10)
+        self.assertEqual(res, 40)
+
+
+if __name__ == "__main__":
+    unittest.ma
