e2d1533afe612671df2259f098568685a013b5c1
Ksenia Ivanova <sweetbananyauwu@gmail.com>
2023-11-23 13:14:42 +0300

    test: added test

--- square.py
+++ square.py
@@ -1,3 +1,5 @@
+import unittest
+
 
 def area(a): # function that returns area of square 
     return a * a # gets two varibles and return one
@@ -5,3 +7,29 @@ def area(a): # function that returns area of square
 
 def perimeter(a): # function that returns perimeter of square 
     return 4 * a # gets two varibles and return one
+
+
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_int_area(self):
+        res = area(10)
+        self.assertEqual(res, 100)
+
+    def test_float_area(self):
+        res = area(5.239)
+        self.assertAlmostEqual(res, 27.447121, delta=0.1)
+
+    def test_zero_perimetr(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_int_perimetr(self):
+        res = perimeter(10)
+        self.assertEqual(res, 40)
+
+    def test_float_perimetr(self):
+        res = perimeter(5.239)
+        self.assertAlmostEqual(res, 20.956, delta=0.1)
