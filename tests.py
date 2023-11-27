238c741e2a8ba5b11be4832a233cb4081c97de39
Vlad-1209 <kriptovlad@gmail.com>
2023-11-27 23:45:03 +0300

    Rename

--- tests.py
+++ tests.py
@@ -15,72 +15,72 @@ from triangle import perimeter as triangle_perimeter
 
 
 class CircleTestCase(unittest.TestCase):
-    def circle_area_test_zero(self):
+    def test_circle_area_zero(self):
         res = circle_area(0)
         self.assertEqual(res, 0)
 
-    def circle_area_test_square(self):
+    def test_circle_area_square(self):
         res = circle_area(5)
         self.assertEqual(res, math.pi * 25)
 
-    def circle_perimeter_test_zero(self):
+    def test_circle_perimeter_zero(self):
         res = circle_perimeter(0)
         self.assertEqual(res, 0)
 
-    def circle_perimeter_test_square(self):
+    def test_circle_perimeter_square(self):
         res = circle_perimeter(5)
         self.assertEqual(res, math.pi * 10)
 
 
 class RectangleTestCase(unittest.TestCase):
-    def rectangle_area_test_zero(self):
+    def test_rectangle_area_zero(self):
         res = rectangle_area(0, 0)
         self.assertEqual(res, 0)
 
-    def rectangle_area_test_square(self):
+    def test_rectangle_area_square(self):
         res = rectangle_area(5, 2)
         self.assertEqual(res, 10)
 
-    def rectangle_perimeter_test_zero(self):
+    def test_rectangle_perimeter_zero(self):
         res = rectangle_perimeter(0, 0)
         self.assertEqual(res, 0)
 
-    def rectangle_perimeter_test_square(self):
+    def test_rectangle_perimeter_square(self):
         res = rectangle_perimeter(5, 4)
         self.assertEqual(res, 18)
 
 
 class SquareTestCase(unittest.TestCase):
-    def square_area_test_zero(self):
+    def test_square_area_zero(self):
         res = square_area(0)
         self.assertEqual(res, 0)
 
-    def square_area_test_square(self):
+    def test_square_area_square(self):
         res = square_area(5)
         self.assertEqual(res, 25)
 
-    def square_perimeter_test_zero(self):
+    def test_square_perimeter_zero(self):
         res = square_perimeter(0)
         self.assertEqual(res, 0)
 
-    def square_perimeter_test_square(self):
+    def test_square_perimeter_square(self):
         res = square_perimeter(5)
         self.assertEqual(res, 20)
 
 
 class TriangleTestCase(unittest.TestCase):
-    def triangle_area_test_zero(self):
+    def test_triangle_area_zero(self):
         res = triangle_area(0, 0)
         self.assertEqual(res, 0)
 
-    def triangle_area_test_square(self):
+    def test_triangle_area_square(self):
         res = triangle_area(5, 2)
         self.assertEqual(res, 10)
 
-    def triangle_perimeter_test_zero(self):
+    def test_triangle_perimeter_zero(self):
         res = triangle_perimeter(0, 0, 0)
         self.assertEqual(res, 0)
 
-    def triangle_perimeter_test_square(self):
+    def test_triangle_perimeter_square(self):
         res = triangle_perimeter(5, 5, 5)
         self.assertEqual(res, 15)
