cee3c60ad6e52988b0227aee31fede59d27cbbd5
Alex Dashkevich <dashkevitcha@yandex.ru>
2023-11-29 14:23:17 +0300

    Add GitHub Action

---
+++ maintest.py
@@ -0,0 +1,8 @@
+from unittest import main
+from test_circle import CircleTestCase
+from test_square import SquareTestCase
+from test_triangle import TriangleTestCase
+from test_rectangle import RectangleTestCase
+
+if __name__ == '__main__':
+    main()
