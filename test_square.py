f443873f5cee781b1abbedd874acd699cab4ac05
kimurtim19 <144263099+kimurtim19@users.noreply.github.com>
2023-11-30 14:31:16 +0300

    Update test_square.py

--- test_square.py
+++ test_square.py
@@ -16,7 +16,7 @@ class SquareTestCase(unittest.TestCase):
     def test_negative_area(self):
         
         res = area(-10)
-        self.assertRaises(res, ExpectedException)
+        self.assertRaises(res, Exception)
 
     def test_zero_perim(self):
         
@@ -26,7 +26,7 @@ class SquareTestCase(unittest.TestCase):
     def test_negative_perimeter(self):
         
         res = perimeter(-10)
-        self.assertRaises(res, ExpectedException)
+        self.assertRaises(res, Exception)
 
     def test_float_perim(self):
         
