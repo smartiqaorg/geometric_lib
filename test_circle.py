6b3b339fbf67fe5e8d64c79f087df5c5047a516a
kimurtim19 <144263099+kimurtim19@users.noreply.github.com>
2023-11-30 13:30:33 +0300

    Update test_circle.py

--- test_circle.py
+++ test_circle.py
@@ -16,7 +16,7 @@ class CircleTestCase(unittest.TestCase):
     def test_float_area(self):
         
         res = area(1337.420)
-        self.assertAlmostEqual(res, 5616493.685096, delta=0.1)
+        self.assertAlmostEqual(res, 5619342.45223919, delta=0.1)
 
     def test_zero_perim(self):
         
@@ -31,4 +31,4 @@ class CircleTestCase(unittest.TestCase):
     def test_float_perim(self):
         
         res = perimeter(1337.420)
-        self.assertAlmostEqual(res, 8398.9976, delta=0.1)
+        self.assertAlmostEqual(res, 8403.257693528, delta=0.1)
