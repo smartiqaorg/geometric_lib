ab407a88e12c056afbb7282e0f710089a0f5c9e2
kimurtim19 <144263099+kimurtim19@users.noreply.github.com>
2023-11-30 13:12:03 +0300

    Update test_rectangle.py

--- test_rectangle.py
+++ test_rectangle.py
@@ -15,6 +15,11 @@ class RectangleTestCase(unittest.TestCase):
         res = area(420.024, 1337.22869)
         self.assertAlmostEqual(res, 561668.143, delta=0.1)
 
+    def test_negative_area(self):
+        
+        res = area(-10, -10)
+        self.assertRaises(res, Exception)
+
     def test_zero_perim(self):
         res = perimeter(0, 0)
         self.assertEqual(res, 0)
@@ -22,6 +27,11 @@ class RectangleTestCase(unittest.TestCase):
     def test_same_numbers_perim(self):
         res = perimeter(10, 10)
         self.assertEqual(res, 40)
+
+    def test_negative_perimeter(self):
+        
+        res = perimeter(-10, -10)
+        self.assertRaises(res, Exception)
 
     def test_float_perim(self):
         res = perimeter(420.024, 1337.22869)
