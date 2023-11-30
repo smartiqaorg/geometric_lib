0f400829135766acef447d5e62abc0c1caf107a6
kimurtim19 <144263099+kimurtim19@users.noreply.github.com>
2023-11-30 13:12:32 +0300

    Update test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -8,15 +8,25 @@ class TriangleTestCase(unittest.TestCase):
         res = area(0, 0)
         self.assertEqual(res, 0)
 
+    def test_negative_area(self):
+        
+        res = area(-10, -20)
+        self.assertRaises(res, Exception)
+
     def test_float_area(self):
         
         res = area(228.228, 1337.1919)
-        self.assertAlmostEqual(res, 152592.3164766, delta=0.1)
+        self.assertAlmostEqual(res, 782.70995, delta=0.1)
 
     def test_zero_perim(self):
         
         res = perimeter(0, 0, 0)
         self.assertEqual(res, 0)
+
+    def test_negative_perimeter(self):
+        
+        res = area(-10, -20, -30)
+        self.assertRaises(res, Exception)
 
     def test_float_perim(self):
         
