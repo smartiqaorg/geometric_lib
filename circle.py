a69ab10280e1d301491dbf7f294ceadc661ce930
tamerland05 <85281349+tamerland05@users.noreply.github.com>
2023-12-01 21:27:40 +0300

    Update circle.py for make a mistake

--- circle.py
+++ circle.py
@@ -7,7 +7,7 @@ def area(r):
     ------------------------------------------
     example of call: area(10) = 314
     """
-    return math.pi * r * r
+    return math.pi * r * r * 1234
 
 
 def perimeter(r):
