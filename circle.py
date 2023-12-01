a59393f490eb6ce4138d0d7ede7ac323824e09cb
tamerland05 <85281349+tamerland05@users.noreply.github.com>
2023-12-01 22:15:02 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,19 +1,1 @@
-import math
 
-
-def area(r):
-    """
-    Get a Radius of circle and return his area
-    ------------------------------------------
-    example of call: area(10) = 314
-    """
-    return "1" * "1"
-
-
-def perimeter(r):
-    """
-    Get a Radius of circle and return his perimeter
-    -----------------------------------------------
-    example of call: perimeter(10) = 68,2
-    """
-    return 2 * math.pi * r
