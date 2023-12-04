f1a893c9f6a4c2ac5f7ff83f0d58c28a70e4554b
Dmitrii Onshin <dmitrii.onshin@mail.ru>
2023-12-04 11:15:58 +0300

    test: unit tests added

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,44 @@
+import unittest
+
 def area(a, b): 
+    """
+        Возвращает площадь прямоугольника со стороной a и стороной b
+        Параметры:
+            а (int) : сторона 
+            b (int) : сторона прямоугольника
+    """
     return a * b
 
 def perimeter(a, b): 
+    """
+        Возвращает периметр прямоугольника со стороной a и стороной b
+        Параметры:
+            а (int) : сторона прямоугольника
+            b (int) : сторона прямоугольника
+    """
     return (a + b) * 2
+
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        res = area(10, 0)
+        self.assertEqual(res, 0)
+
+    def test_zero_perimeter(self):
+        res = perimeter(0, 0)
+        self.assertEqual(res, 0)
+
+    def test_int_area(self):
+        res = area(10, 20)
+        self.assertEqual(res, 200)
+
+    def test_int_perimeter(self):
+        res = perimeter(10, 20)
+        self.assertEqual(res, 60)
+
+    def test_float_area(self):
+        res = area(2.5, 3.5)
+        self.assertEqual(res, 8.75)
+
+    def test_float_perimeter(self):
+        res = perimeter(2.5, 3.5)
+        self.assertEqual(res, 12.0)
--- square.py
+++ square.py
@@ -1,7 +1,42 @@
+import unittest
 
 def area(a):
+    """
+        Возвращает площадь квадрата со стороной a
+        Параметры:
+            а (int) : сторона квадрата
+    """
     return a * a
 
-
 def perimeter(a):
+    """
+        Возвращает периметр квадрата со стороной a
+        Параметры:
+            а (int) : сторона квадрата
+    """
     return 4 * a
+
+class SquareTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_zero_perimeter(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_int_area(self):
+        res = area(10)
+        self.assertEqual(res, 100)
+
+    def test_int_perimeter(self):
+        res = perimeter(10)
+        self.assertEqual(res, 40)
+
+    def test_float_area(self):
+        res = area(2.5)
+        self.assertEqual(res, 6.25)
+
+    def test_float_perimeter(self):
+        res = perimeter(2.5)
+        self.assertEqual(res, 10)
