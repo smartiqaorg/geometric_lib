d4c9112070deae64fd773a01c3838dfa5f3c8dba
Dmitrii Onshin <dmitrii.onshin@mail.ru>
2023-12-05 22:10:25 +0300

    refactor: moved tests to test files

--- circle.py
+++ circle.py
@@ -16,28 +16,3 @@ def perimeter(r):
             r (int) : радиус окружности
     """
     return 2 * math.pi * r
-
-class CircleTestCase(unittest.TestCase):
-    def test_zero_area(self):
-        res = area(0)
-        self.assertEqual(res, 0)
-
-    def test_zero_perimeter(self):
-        res = perimeter(0)
-        self.assertEqual(res, 0)
-
-    def test_int_area(self):
-        res = area(10)
-        self.assertEqual(res, 314.1592653589793)
-
-    def test_int_perimeter(self):
-        res = perimeter(10)
-        self.assertEqual(res, 62.83185307179586)
-
-    def test_float_area(self):
-        res = area(2.5)
-        self.assertEqual(res, 19.634954084936208)
-
-    def test_float_perimeter(self):
-        res = perimeter(2.5)
-        self.assertEqual(res, 15.707963267948966)
