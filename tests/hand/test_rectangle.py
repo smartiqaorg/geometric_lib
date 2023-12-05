c9c2e07b6941125706096e9adeebf94bf0054611
mashiklavashik <409162@niuitmo.ru>
2023-12-05 14:52:50 +0300

    Fix: error in output formatting test_rectangle.py

--- tests/hand/test_rectangle.py
+++ tests/hand/test_rectangle.py
@@ -18,7 +18,7 @@ def test_rectangle_perimeter():
     for i in range(len(test_input)):
 
         if test_input[i][0] <= 0 or test_input[i][1] <= 0:
-            print(f"AREA TEST №{i + 1}: invalid value entered\n")
+            print(f"PERIMETER TEST №{i + 1}: invalid value entered\n")
 
         elif rectangle.perimeter(test_input[i][0], test_input[i][1]) == test_answers_perimeter[i]:
             print(f"PERIMETER TEST №{i + 1}: correct\n")
@@ -35,7 +35,7 @@ def test_rectangle_perimeter():
 
 test_input = [[5, -7], [8, 13.5], [1, 3], [0, 89]]
 test_answers_area = ["error", 108.0, 3, 0]
-test_answers_perimeter = [24, 43.0, 8, 0]
+test_answers_perimeter = ["error", 43.0, 8, "error"]
 
 test_rectangle_area()
 test_rectangle_perimeter()
