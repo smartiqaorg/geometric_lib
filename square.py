34ed2bd21168e7c59dcd586de4ba977ded161c90
Amalia Tereshenko <144378797+mirreael@users.noreply.github.com>
2023-12-05 10:38:08 +0300

    test: added tests

--- square.py
+++ square.py
@@ -1,3 +1,4 @@
+import unittest
 
 def area(a):
     """
@@ -15,3 +16,28 @@ def perimeter(a):
             а (int) : сторона квадрата
     """
     return 4 * a
+
+class SquareTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_zero_perimeter(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_int_area(self):
+        res = area(5)
+        self.assertEqual(res, 25)
+
+    def test_int_perimeter(self):
+        res = perimeter(5)
+        self.assertEqual(res, 20)
+
+    def test_float_area(self):
+        res = area(5.5)
+        self.assertEqual(res, 30.25)
+
+    def test_float_perimeter(self):
+        res = perimeter(1.5)
+        self.assertEqual(res, 6.0)
