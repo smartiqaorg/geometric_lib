5750c304ebd3cfb87a8bce05110dab95b3d07739
mashiklavashik <409162@niuitmo.ru>
2023-12-05 14:36:47 +0300

    Update: add tests test_rectangle.py

--- tests/hand/test_rectangle.py
+++ tests/hand/test_rectangle.py
@@ -3,26 +3,28 @@ import rectangle
 
 def test_rectangle_area():
     for i in range(len(test_input)):
-        if rectangle.area(test_input[i][0], test_input[i][1]) == test_answers_area[i]:
-            print(f"AREA TEST №{i + 1}")
-            print(f"INPUT:  {test_input[i][0]} {test_input[i][1]}")
-            print(f"TRUE: {test_answers_area[i]} = {test_answers_area[i]}\n")
+
+        if test_input[i][0] < 0 or test_input[i][1] < 0:
+            print(f"AREA TEST №{i + 1}: invalid value entered\n")
+
+        elif rectangle.area(test_input[i][0], test_input[i][1]) == test_answers_area[i]:
+            print(f"AREA TEST №{i + 1}: correct\n")
+
         else:
-            print(f"AREA TEST №{i + 1}")
-            print(f"INPUT: {test_input[i][0]} {test_input[i][1]}")
-            print(f"FALSE: {rectangle.area(test_input[i][0], test_input[i][1])} != {test_answers_area[i]}\n")
+            print(f"AREA TEST №{i + 1}: calculation error\n")
 
 
 def test_rectangle_perimeter():
     for i in range(len(test_input)):
-        if rectangle.perimeter(test_input[i][0], test_input[i][1]) == test_answers_perimeter[i]:
-            print(f"PERIMETER TEST №{i + 1}")
-            print(f"INPUT:  {test_input[i][0]} {test_input[i][1]}")
-            print(f"TRUE: {test_answers_perimeter[i]} = {test_answers_perimeter[i]}\n")
+
+        if test_input[i][0] <= 0 or test_input[i][1] <= 0:
+            print(f"AREA TEST №{i + 1}: invalid value entered\n")
+
+        elif rectangle.perimeter(test_input[i][0], test_input[i][1]) == test_answers_perimeter[i]:
+            print(f"PERIMETER TEST №{i + 1}: correct\n")
+
         else:
-            print(f"PERIMETER TEST №{i + 1}")
-            print(f"INPUT: {test_input[i][0]} {test_input[i][1]}")
-            print(f"FALSE: {rectangle.perimeter(test_input[i][0], test_input[i][1])} != {test_answers_perimeter[i]}\n")
+            print(f"PERIMETER TEST №{i + 1}: calculation error\n")
 
 
 '''
@@ -31,9 +33,9 @@ def test_rectangle_perimeter():
         b - длина двух сторон
                                                     '''
 
-test_input = [[5, 7], [8, 13], [1, 3]]
-test_answers_area = [35, 104, 3]
-test_answers_perimeter = [24, 34, 8]
+test_input = [[5, -7], [8, 13.5], [1, 3], [0, 89]]
+test_answers_area = ["error", 108.0, 3, 0]
+test_answers_perimeter = [24, 43.0, 8, 0]
 
 test_rectangle_area()
 test_rectangle_perimeter()
