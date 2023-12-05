f031632598c6171eb754534945c66699bac34089
ssandaa <144237596+ssandaa@users.noreply.github.com>
2023-12-05 23:23:38 +0300

    Add files via upload

---
+++ triangle_test.py
@@ -0,0 +1,38 @@
+import unittest
+from triangle import area, perimeter
+class TriangleTestCase(unittest.TestCase):
+    def test_triangle_area_norm(self):
+        res = area(4, 5)
+        self.assertEqual(res, 10.0)
+
+    def test_triangle_area_neNorm(self):
+        res = area(4, -5)
+        self.assertEqual(res, 'the base and height cannot be negative')
+
+    def test_triangle_area_real(self):
+        res = area(4.5, 5.5)
+        self.assertEqual(res, 12.375)
+
+    def test_triangle_area_zero(self):
+        res = area(4, 0)
+        self.assertEqual(res, 0)
+
+    def test_triangle_perimeter_norm(self):
+        res = perimeter(4, 5, 6)
+        self.assertEqual(res, 15)
+
+    def test_triangle_perimeter_neNorm(self):
+        res = perimeter(4, 5, -6)
+        self.assertEqual(res, 'the base and height cannot be negative')
+
+    def test_triangle_perimeter_real(self):
+        res = perimeter(4.5, 5.5, 6.5)
+        self.assertEqual(res, 16.5)
+
+    def test_triangle_perimeter_zero(self):
+        res = perimeter(4, 0, 4.5)
+        self.assertEqual(res, 8.5)
+
+
+if __name__ == '__main__':
+    unittest.main()
