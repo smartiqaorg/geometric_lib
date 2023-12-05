1514138c0903576f38db2500618a3efb2e948792
Amalia Tereshenko <144378797+mirreael@users.noreply.github.com>
2023-12-05 21:31:14 +0300

    refactor: deleted tests

--- rectangle.py
+++ rectangle.py
@@ -18,27 +18,3 @@ def perimeter(a, b):
     """
     return 2*a + 2*b
 
-class RectangleTestCase(unittest.TestCase):
-    def test_zero_area(self):
-        res = area(10, 0)
-        self.assertEqual(res, 0)
-
-    def test_zero_perimeter(self):
-        res = perimeter(0, 0)
-        self.assertEqual(res, 0)
-
-    def test_int_area(self):
-        res = area(20, 20)
-        self.assertEqual(res, 400)
-
-    def test_int_perimeter(self):
-        res = perimeter(10, 10)
-        self.assertEqual(res, 40)
-
-    def test_float_area(self):
-        res = area(2.0, 3.5)
-        self.assertEqual(res, 7.0)
-
-    def test_float_perimeter(self):
-        res = perimeter(2.5, 3.5)
-        self.assertEqual(res, 12.0)
--- triangle.py
+++ triangle.py
@@ -18,28 +18,3 @@ def perimeter(a, b, c):
             c (int) : сторона треугольника
     """
     return a + b + c
-
-class TriangleTestCase(unittest.TestCase):
-    def test_zero_area(self):
-        res = area(0, 0)
-        self.assertEqual(res, 0)
-
-    def test_zero_perimeter(self):
-        res = perimeter(0, 0, 0)
-        self.assertEqual(res, 0)
-
-    def test_int_area(self):
-        res = area(5, 8)
-        self.assertEqual(res, 20)
-
-    def test_int_perimeter(self):
-        res = perimeter(7, 4, 10)
-        self.assertEqual(res, 21)
-
-    def test_float_area(self):
-        res = area(3.5, 2.5)
-        self.assertEqual(res, 4.375)
-
-    def test_float_perimeter(self):
-        res = perimeter(2.5, 2.5, 3.2)
-        self.assertEqual(res, 8.2)
