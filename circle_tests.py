e45430394f81ae2877dc2dc7586c9e58013d4249
kashasnish <144245517+kashasnish@users.noreply.github.com>
2023-12-13 01:49:14 +0300

    Update circle_tests.py

--- circle_tests.py
+++ circle_tests.py
@@ -1,7 +1,7 @@
 import unittest
 import math
 
-import geometric_lib.circle
+from circle import *
 
 
 class TestCircleFunctions(unittest.TestCase):
@@ -9,12 +9,12 @@ class TestCircleFunctions(unittest.TestCase):
         # Проверка правильности вычисления площади круга
         radius = 5
         expected_area = math.pi * radius * radius
-        result = geometric_lib.circle.area(radius)
+        result = area(radius)
         self.assertEqual(result, expected_area, f"Ожидаемая площадь: {expected_area}, Фактическая площадь: {result}")
 
     def test_perimeter(self):
         # Проверка правильности вычисления периметра круга
         radius = 5
         expected_perimeter = 2 * math.pi * radius
-        result = geometric_lib.circle.perimeter(radius)
+        result = perimeter(radius)
         self.assertEqual(result, expected_perimeter, f"Ожидаемый периметр: {expected_perimeter}, Фактический периметр: {result}")
