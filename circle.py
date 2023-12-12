bd9f609fd9992b904f25d8ca8aa0bc4da10a21c6
Валерий Николаевич Пряжников <144845869+SNDSG@users.noreply.github.com>
2023-12-13 00:33:17 +0300

    s
    TESTS

--- circle.py
+++ circle.py
@@ -7,23 +7,3 @@ def area(r):
 
 def perimeter(r):
     return 2 * math.pi * r
-
-<<<<<<< Updated upstream
-=======
-class CircleTestCase(unittest.TestCase):
-    def test_area_zero_r(self):
-        res = area(0)
-        self.assertEqual(res, 0)
-
-    def test_area_one(self):
-        res = area(1)
-        self.assertEqual(res, math.pi)
-
-    def test_perimetr_zero(self):
-        res = perimeter(0)
-        self.assertEqual(res, 0)
-
-    def test_perimetr_one(self):
-        res = perimeter(1)
-        self.assertEqual(res, 2*math.pi)
->>>>>>> Stashed changes
