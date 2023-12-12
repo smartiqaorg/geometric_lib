b590b6cf1e6972464a45276291961911b9b1e452
fgiklfj <minskaya.anastasia@yandex.ru>
2023-12-12 19:11:38 +0300

    добавлены unit тесты для каждой функции

---
+++ maintest.py
@@ -0,0 +1,8 @@
+from unittest import main
+from circle_test import CircleTestCase
+from square_test import SquareTestCase
+from rectangle_test import RectangleTestCase
+from triangle_test import TriangleTestCase
+
+if __name__ == '__main__':
+    main()
