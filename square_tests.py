cc9064991d65de4ef7ba511702c71436934ecf7a
kashasnish <144245517+kashasnish@users.noreply.github.com>
2023-12-13 01:51:48 +0300

    Update square_tests.py

--- square_tests.py
+++ square_tests.py
@@ -1,5 +1,5 @@
 import unittest
-import geometric_lib.square
+from square import *
 
 
 class TestSquareFunctions(unittest.TestCase):
@@ -7,12 +7,12 @@ class TestSquareFunctions(unittest.TestCase):
         # Проверка правильности вычисления площади квадрата
         side_length = 5
         expected_area = side_length * side_length
-        result = geometric_lib.square.area(side_length)
+        result = area(side_length)
         self.assertEqual(result, expected_area, f"Ожидаемая площадь: {expected_area}, Фактическая площадь: {result}")
 
     def test_perimeter(self):
         # Проверка правильности вычисления периметра квадрата
         side_length = 5
         expected_perimeter = 4 * side_length
-        result = geometric_lib.square.perimeter(side_length)
+        result = perimeter(side_length)
         self.assertEqual(result, expected_perimeter,f"Ожидаемый периметр: {expected_perimeter}, Фактический периметр: {result}")
