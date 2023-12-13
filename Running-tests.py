62e4555ddc65e62f72da5e56b3bbbf387f838c66
EgorSandalov <egorsandalov72@gmail.com>
2023-12-13 06:01:13 +0300

    Добавил функцию вызова тестов

---
+++ Running-tests.py
@@ -0,0 +1,11 @@
+from unittest import main
+import sys
+sys.path.append('C:/Users/Банан/geometric_lib/tests')
+
+from test_circle import Testcircle
+from test_rectangle import TestRectangle
+from test_square import TestSquare
+from test_triangle import TestTriangle
+
+if __name__ == "__main__":
+    main()
