78e8f26beb479760a11cdf6ea2120e3ca3f84146
Dre9y <xxdregteilsxx@gmail.com>
2023-12-14 13:03:32 +0300

    updated exeptiins

--- circle.py
+++ circle.py
@@ -1,24 +1,31 @@
 import math
 
-
 def area(r):
-    return math.pi * r * r
+    return math.pi* r*r
 
 
 def perimeter(r):
-    return 2 * math.pi * r
+    return math.pi* 2*r
 
-def test_circle_area(self):
-    self.assertEqual(area(2), 12.566)
-    self.assertEqual(area(0), 0)
-    self.assertEqual(area(5), 78.359)
-def test_cicrle_perimeter(self):
-    self.assertEqual(perimeter(2), 4)
-    self.assertEqual(perimeter(0), 0)
-    self.assertEqual(perimeter(5), 25)
+import unittest
 
-def test_circle_area_negative(self):
-    self.assertEqual(TypeError,perimeter, -1)
+class TestArea(unittest.TestCase):
+        def test_circle_area(self):
+            self.assertEqual(area(2), 12.566)
+            self.assertEqual(area(0), 0)
+            self.assertEqual(area(5), 78.359)
 
-def test_circle_area_negative(self):
-    self.assertEqual(TypeError,area, -1)
+        def test_circle_area_negative(self):
+            self.assertEqual(TypeError,perimeter, -1)
+
+class TestPerimeter(unittest.TestCase):
+        def test_cicrle_perimeter(self):
+            self.assertEqual(perimeter(2), 12.566)
+            self.assertEqual(perimeter(0), 0)
+            self.assertEqual(perimeter(5), 25)
+
+        def test_circle_area_negative(self):
+          self.assertEqual(TypeError,area, -1)
+
+        def test_circle_area_string(self):
+            self.assertEqual(TypeError,area, 'abc')
--- rectangle.py
+++ rectangle.py
@@ -14,6 +14,11 @@ class TestArea(unittest.TestCase):
         self.assertEqual(area(5, 5), 25)
         self.assertEqual(area(-1, 4), -4)
 
+    def test_rectangle_area_negative(self):
+        self.assertEqual(TypeError,perimeter,0, -1)
+    def test_circle_area_string(self):
+        self.assertEqual(TypeError,area, 'abc',7)
+
 
 class TestPerimeter(unittest.TestCase):
     def test_perimeter(self):
@@ -21,3 +26,7 @@ class TestPerimeter(unittest.TestCase):
         self.assertEqual(perimeter(5, 5), 20)
         self.assertEqual(perimeter(0, 0), 0)
         self.assertEqual(perimeter(-2, 4), 4)
+    def test_rectangle_area_negative(self):
+        self.assertEqual(TypeError,perimeter, -1)
+
+
