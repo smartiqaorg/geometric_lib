de73ff98ade823ba8ab82674ff8c72c2b7d0e523
asgard1999 <145256913+asgard1999@users.noreply.github.com>
2023-12-14 12:05:30 +0300

    Mistake in  test_circle.py corrected

--- test_circle.py
+++ test_circle.py
@@ -1,37 +1,26 @@
 import unittest
-from circle import area,perimeter
 
-class CircleTestCase(unittest.TestCase):
-    def test_circle_area_dobri(self):
-        res = area(5)
-        self.assertEqual(res, 78.53981633974483)
+def test_area():
+    assert area(1) == 3.141592653589793
+    assert area(133.41) == 55914.78264587542
+    assert area(-4) == Exception
 
-    def test_circle_area_zloy(self):
-        res = area(-5)
-        self.assertEqual(res, 'The radius cannot be negative')
+def test_perimeter():
+    assert perimeter(1) == 6.283185307179586
+    assert perimeter(133.41) == 838.2397518308286
+    assert perimeter(-4) == Exception
 
-    def test_circle_area_real(self):
-        res = area(6.5)
-        self.assertEqual(res, 132.73228961416876)
+class TestCircle(unittest.TestCase):
 
-    def test_circle_area_zero(self):
-        res = area(0)
-        self.assertEqual(res, 0)
+    def test_area(self):
+        self.assertEqual(area(1), 3.141592653589793)
+        self.assertEqual(area(133.41), 55914.78264587542)
+        self.assertRaises(area(-4), Exception)
 
-    def test_circle_perimeter_dobri(self):
-        res = perimeter(5)
-        self.assertEqual(res, 31.41592653589793)
+    def test_perimeter(self):
+        self.assertEqual(perimeter(1), 6.283185307179586)
+        self.assertEqual(perimeter(133.41), 838.2397518308286)
+        self.assertRaises(perimeter(-4), Exception)
 
-    def test_circle_perimeter_zloy(self):
-        res = perimeter(-5)
-        self.assertEqual(res, 'The radius cannot be negative')
-
-    def test_circle_perimeter_real(self):
-        res = perimeter(6.5)
-        self.assertEqual(res, 40.840704496667314)
-
-    def test_circle_perimeter_zero(self):
-        res = perimeter(0)
-        self.assertEqual(res, 0)
 if __name__ == '__main__':
     unittest.main()
