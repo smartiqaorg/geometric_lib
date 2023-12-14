b56278eabb5cefcf3ccc12874b7f66b54a84e00f
asgard1999 <145256913+asgard1999@users.noreply.github.com>
2023-12-14 12:24:04 +0300

    Mistake in test_square.py is corrected

--- test_square.py
+++ test_square.py
@@ -1,32 +1,37 @@
 import unittest
+from square import area, perimeter
 
-def area(a):
-    return a * a
+class SquareTestCase(unittest.TestCase):
+    def test_square_area_norm(self):
+        res = area(4)
+        self.assertEqual(res, 16)
 
-def perimeter(a):
-    return 4 * a
+    def test_square_area_neNorm(self):
+        with self.assertEqual(ValueError):
+            area(-4)
 
-def test_area():
-    assert area(2) == 4
-    assert area(3.3) == 10.889999999999999
-    assert area(-2) == Exception
+    def test_square_area_real(self):
+        res = area(4.5)
+        self.assertEqual(res, 20.25)
 
-def test_perimeter():
-    assert perimeter(2) == 8
-    assert perimeter(3.3) == 13.2
-    assert perimeter(-2) == Exception
+    def test_square_area_zero(self):
+        res = area(0)
+        self.assertEqual(res, 0)
 
-class TestRectangle(unittest.TestCase):
+    def test_square_perimeter_norm(self):
+        res = perimeter(4)
+        self.assertEqual(res, 16)
 
-    def test_area(self):
-        self.assertEqual(area(2), 4)
-        self.assertEqual(area(3.3), 10.889999999999999)
-        self.assertRaises(area(-2), Exception)
+    def test_square_perimeter_neNorm(self):
+        with self.assertEqual(ValueError):
+            perimeter(-4)
 
-    def test_perimeter(self):
-        self.assertEqual(perimeter(2), 8)
-        self.assertEqual(perimeter(3.3), 13.2)
-        self.assertRaises(perimeter(-2), Exception)
+    def test_square_perimeter_real(self):
+        res = perimeter(4.5)
+        self.assertEqual(res, 18.0)
 
-if __name__ == '__main__':
+    def test_square_perimeter_zero(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+if name == '__main__':
     unittest.main()
