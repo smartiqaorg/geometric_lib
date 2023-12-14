ccb8e0c5dae0ce28d61dcba3d99ae6e9e2e82426
asgard1999 <145256913+asgard1999@users.noreply.github.com>
2023-12-14 12:25:17 +0300

    Mistake in test_rectangle.py is corrected

--- test_rectangle.py
+++ test_rectangle.py
@@ -1,37 +1,37 @@
 import unittest
+from rectangle import area, perimeter
+class RectangleTestCase(unittest.TestCase):
+    def test_rectangle_area_norm(self):
+        res = area(4, 5)
+        self.assertEqual(res, 20)
 
-def area(a, b):
-    return a * b
+    def test_rectangle_area_neNorm(self):
+        with self.assertEqual(ValueError):
+            area(4, -5)
 
-def perimeter(a, b):
-    return (a + b) * 2
+    def test_rectangle_area_real(self):
+        res = area(4.5, 5.5)
+        self.assertEqual(res, 24.75)
 
-def test_area():
-    assert area(2, 3) == 6
-    assert area(4.6, 5.6) == 25.76
-    assert area(-2, -3) == Exception
+    def test_rectangle_area_zero(self):
+        res = area(4, 0)
+        self.assertEqual(res, 0)
 
-def test_perimeter():
-    assert perimeter(2, 3) == 10
-    assert perimeter(4.6, 5.6) == 20.4
-    assert perimeter(-2, -3) == Exception
+    def test_rectangle_perimeter_norm(self):
+        res = perimeter(4, 5)
+        self.assertEqual(res, 18)
 
-if __name__ == '__main__':
-    test_area()
-    test_perimeter()
+    def test_rectangle_perimeter_neNorm(self):
+        with self.assertEqual(rValueError):
+            perimeter(4, -5)
 
-class TestRectangle(unittest.TestCase):
+    def test_rectangle_perimeter_real(self):
+        res = perimeter(4.5, 5.5)
+        self.assertEqual(res, 20.0)
 
-    def test_area(self):
-        self.assertEqual(area(2, 3), 6)
-        self.assertEqual(area(4, 5), 20)
-        self.assertRaises(area(-2, -3), Exception)
+    def test_rectangle_perimeter_zero(self):
+        res =  perimeter(4, 0)
+        self.assertEqual(res, 😍
 
-    def test_perimeter(self):
-        self.assertEqual(perimeter(2, 3), 10)
-        self.assertEqual(perimeter(4, 5), 18)
-        self.assertRaises(perimeter(-2, -3), Exception)
-
-if __name__ == '__main__':
+if name == '__main__':
     unittest.main()
-
