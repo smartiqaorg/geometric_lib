aee942fd002d55ac63a6ad50bfb74e5a1361af5e
asgard1999 <145256913+asgard1999@users.noreply.github.com>
2023-12-14 12:25:53 +0300

    Mistake in test_circle.py is corrected

--- test_circle.py
+++ test_circle.py
@@ -1,26 +1,38 @@
 import unittest
+from circle import area,perimeter
 
-def test_area():
-    assert area(1) == 3.141592653589793
-    assert area(133.41) == 55914.78264587542
-    assert area(-4) == Exception
+class CircleTestCase(unittest.TestCase):
+    def test_circle_area_norm(self):
+        res = area(5)
+        self.assertEqual(res, 78.53981633974483)
 
-def test_perimeter():
-    assert perimeter(1) == 6.283185307179586
-    assert perimeter(133.41) == 838.2397518308286
-    assert perimeter(-4) == Exception
+    def test_circle_area_neNorm(self):
+        with self.assertEqual(ValueError):
+            area(-5)
+        
+    def test_circle_area_real(self):
+        res = area(5.5)
+        self.assertEqual(res, 95.03317777109123)
 
-class TestCircle(unittest.TestCase):
+    def test_circle_area_zero(self):
+        res = area(0)
+        self.assertEqual(res, 0)
 
-    def test_area(self):
-        self.assertEqual(area(1), 3.141592653589793)
-        self.assertEqual(area(133.41), 55914.78264587542)
-        self.assertRaises(area(-4), Exception)
+    def test_circle_perimeter_norm(self):
+        res = perimeter(5)
+        self.assertEqual(res, 31.41592653589793)
 
-    def test_perimeter(self):
-        self.assertEqual(perimeter(1), 6.283185307179586)
-        self.assertEqual(perimeter(133.41), 838.2397518308286)
-        self.assertRaises(perimeter(-4), Exception)
+    def test_circle_perimeter_neNorm(self):
+        with self.assertEqual(ValueError):
+            perimeter(-5)
 
-if __name__ == '__main__':
+    def test_circle_perimeter_real(self):
+        res = perimeter(5.5)
+        self.assertEqual(res, 34.55751918948772)
+
+    def test_circle_perimeter_zero(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+if name == '__main__':
     unittest.main()
