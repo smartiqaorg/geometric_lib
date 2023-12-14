4993ee085c79712533049f3cd79e682c2a05fe43
Edward <edwarddem1346@gmail.com>
2023-12-14 14:02:31 +0300

    tests: added more tests to circle, rectangle, square and triangle modules

--- tests/unit/rectangle_test.py
+++ tests/unit/rectangle_test.py
@@ -4,15 +4,25 @@ from rectangle import area, perimeter
 
 
 class RectangleTest(unittest.TestCase):
-    def test_area(self):
+    def test_area_correct(self):
         self.assertAlmostEqual(area(8, 9), 72)
         self.assertAlmostEqual(area(3, 7), 21)
-        self.assertAlmostEqual(area(-2, 89), 178)
 
-    def test_perimeter(self):
+    def test_area_value_error(self):
+        self.assertRaises(ValueError, area(-2, 89))
+
+    def test_area_type_error(self):
+        self.assertRaises(TypeError, area("str"))
+
+    def test_perimeter_correct(self):
         self.assertAlmostEqual(perimeter(9.4, 8), 34.8)
         self.assertAlmostEqual(perimeter(10, 12), 44)
-        self.assertAlmostEqual(perimeter(-6, 84), 180)
+
+    def test_area_value_error(self):
+        self.assertRaises(ValueError, perimeter(-6, 84))
+
+    def test_area_type_error(self):
+        self.assertRaises(TypeError, perimeter("str"))
 
 
 if __name__ == '__main__':
