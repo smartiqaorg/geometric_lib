a6137176b6a8880a160e30557e9f269c7552dc0c
asgard1999 <145256913+asgard1999@users.noreply.github.com>
2023-12-14 13:13:24 +0300

    test: mistake in  test_circle.py is corrected

--- test_circle.py
+++ test_circle.py
@@ -4,15 +4,15 @@ from circle import area,perimeter
 class CircleTestCase(unittest.TestCase):
     def test_circle_area_norm(self):
         res = area(5)
-        self.assertEqual(res, 78.540)
+        self.assertAlmostEqual(res, 78.540)
 
     def test_circle_area_neNorm(self):
-        with self.assertEqual(ValueError):
+        with self.assertRaises(ValueError):
             area(-6)
         
     def test_circle_area_real(self):
         res = area(5.5)
-        self.assertEqual(res, 95.033)
+        self.assertAlmostEqual(res, 95.033)
 
     def test_circle_area_zero(self):
         res = area(0)
@@ -20,15 +20,15 @@ class CircleTestCase(unittest.TestCase):
 
     def test_circle_perimeter_norm(self):
         res = perimeter(5)
-        self.assertEqual(res, 31.416)
+        self.assertAlmostEqual(res, 31.416)
 
     def test_circle_perimeter_neNorm(self):
-        with self.assertEqual(ValueError):
+        with self.assertRaises(ValueError):
             perimeter(-5)
 
     def test_circle_perimeter_real(self):
         res = perimeter(5.5)
-        self.assertEqual(res, 34.558)
+        self.assertAlmostEqual(res, 34.558)
 
     def test_circle_perimeter_zero(self):
         res = perimeter(0)
