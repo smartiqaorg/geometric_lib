86a8dbf051ab628abffa8103f0acc98f0b689915
Dre9y <xxdregteilsxx@gmail.com>
2023-12-14 12:26:14 +0300

    added unit test with exeption types

--- circle.py
+++ circle.py
@@ -8,3 +8,17 @@ def area(r):
 def perimeter(r):
     return 2 * math.pi * r
 
+def test_circle_area(self):
+    self.assertEqual(area(2), 12.566)
+    self.assertEqual(area(0), 0)
+    self.assertEqual(area(5), 78.359)
+def test_cicrle_perimeter(self):
+    self.assertEqual(perimeter(2), 4)
+    self.assertEqual(perimeter(0), 0)
+    self.assertEqual(perimeter(5), 25)
+
+def test_circle_area_negative(self):
+    self.assertEqual(TypeError,perimeter, -1)
+
+def test_circle_area_negative(self):
+    self.assertEqual(TypeError,area, -1)
