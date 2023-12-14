06ec2a45344dabfb3e099c2fcea3e80d1dd5674a
aidashovv <144238274+aidashovv@users.noreply.github.com>
2023-12-14 12:30:38 +0300

    test: add test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -10,15 +10,27 @@ class TriangleTestCase(t.TestCase):
 
     def test_negative_mul_negative(self):
         res = area(-1, -1)
-        self.assertEqual(res, None)
+        self.assertEqual(res, 0.5)
+
+    def test_negative_mul_negative_error(self):
+        with self.assertEqual(ValueError):
+            area(-1, -1)
 
     def test_float_mul_x(self):
         res = area(1.75, 4)
         self.assertEqual(res, 3.5)
 
-    def test_does_it_exist(self):
+    def test_posx3(self):
         res = perimeter(2, 3, 4)
-        self.assertEqual(res, 'does not exist')
+        self.assertEqual(res, 9)
+
+    def test_posx3_error(self):
+        with self.assertEqual(ValueError):
+            area(2, 3, 4)
+
+    def test_negative_mul_negative_error(self):
+        with self.assertEqual(ValueError):
+            area(-1, -1)
 
     def test_triple_one(self):
         res = perimeter(1, 1, 1)
@@ -26,4 +38,9 @@ class TriangleTestCase(t.TestCase):
 
     def test_negative_perimeter(self):
         res = perimeter(12, 15, -19)
-        self.assertEqual(res, None)
+        self.assertEqual(res, 8)
+
+    def test_negative_perimeter_error(self):
+        with self.assertEqual(ValueError):
+            area(12, 15, -19)
+
