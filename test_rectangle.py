56b98116ac0f59031625d201f95645c0c99ac848
aidashovv <144238274+aidashovv@users.noreply.github.com>
2023-12-14 12:35:01 +0300

    test: add test_rectangle.py

--- test_rectangle.py
+++ test_rectangle.py
@@ -6,7 +6,11 @@ from rectangle import perimeter
 class RectangleTestCase(t.TestCase):
     def test_negative_mul_positive(self):
         res = area(-3, 3)
-        self.assertEqual(res, None)
+        self.assertEqual(res, -9)
+
+    def test_negative_mul_positive_error(self):
+        with self.assertEqual(ValueError):
+            area(-3, 3)
 
     def test_zero_mul_x(self):
         res = area(0, 3)
@@ -18,7 +22,11 @@ class RectangleTestCase(t.TestCase):
 
     def test_negative_and_positive(self):
         res = perimeter(-13, 12)
-        self.assertEqual(res, None)
+        self.assertEqual(res, -2)
+
+    def test_negative_and_positive_error(self):
+        with self.assertEqual(ValueError):
+            area(-13, 12)
 
     def test_zero_and_zero(self):
         res = perimeter(0, 0)
