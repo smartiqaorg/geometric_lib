2300ed25221a79a240aa26d4e469e82e577d9460
Edward <edwarddem1346@gmail.com>
2023-12-14 14:20:01 +0300

    fix: fixed tests execution and added tests

--- tests/unit/circle_test.py
+++ tests/unit/circle_test.py
@@ -9,20 +9,20 @@ class CircleTest(unittest.TestCase):
 		self.assertAlmostEqual(area(4), 50.266, delta=0.001)
 
 	def test_area_value_error(self):
-		self.assertRaises(ValueError, area(-34))
+		self.assertRaises(ValueError, area, -34)
 
 	def test_area_type_error(self):
-		self.assertRaises(TypeError, area("str"))
+		self.assertRaises(TypeError, area, "str")
 
 	def test_perimeter_correct(self):
 		self.assertAlmostEqual(perimeter(4), 25.133, delta=0.001)
 		self.assertAlmostEqual(perimeter(8), 50.266, delta=0.001)
 
 	def test_perimeter_value_error(self):
-		self.assertRaises(ValueError, perimeter(-15))
+		self.assertRaises(ValueError, perimeter, -15)
 
-	def test_area_type_error(self):
-		self.assertRaises(TypeError, perimeter("str"))
+	def test_perimeter_type_error(self):
+		self.assertRaises(TypeError, perimeter, "str")
 
 
 if __name__ == "__main__":
--- tests/unit/triangle_test.py
+++ tests/unit/triangle_test.py
@@ -9,20 +9,20 @@ class TriangleTest(unittest.TestCase):
         self.assertAlmostEqual(area(3, 10), 15)
 
     def test_area_value_error(self):
-        self.assertRaises(ValueError, area(-2, 2))
+        self.assertRaises(ValueError, area, -2, 2)
 
     def test_area_type_error(self):
-        self.assertRaises(TypeError, area("str"))
+        self.assertRaises(TypeError, area, 2, "str")
 
     def test_perimeter_correct(self):
         self.assertAlmostEqual(perimeter(3, 4, 5), 12)
         self.assertAlmostEqual(perimeter(10, 12, 18), 40)
 
     def tests_perimeter_value_error(self):
-        self.assertRaises(ValueError, perimeter(-8, 8, 9))
+        self.assertRaises(ValueError, perimeter, -8, 8, 9)
 
     def tests_perimeter_type_error(self):
-        self.assertRaises(TypeError, perimeter("str", 8, 8))
+        self.assertRaises(TypeError, perimeter, "str", 8, 8)
 
 
 if __name__ == '__main__':
