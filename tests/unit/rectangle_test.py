85ec7dbf3497af247888ef317d2c2c4f1c7bb88d
Edward <edwarddem1346@gmail.com>
2023-12-14 14:43:30 +0300

    fix: fixed tests execution code

--- tests/unit/rectangle_test.py
+++ tests/unit/rectangle_test.py
@@ -9,20 +9,20 @@ class RectangleTest(unittest.TestCase):
         self.assertAlmostEqual(area(3, 7), 21)
 
     def test_area_value_error(self):
-        self.assertRaises(ValueError, area(-2, 89))
+        self.assertRaises(ValueError, area, -2, 89)
 
     def test_area_type_error(self):
-        self.assertRaises(TypeError, area("str"))
+        self.assertRaises(TypeError, area, -2, "str")
 
     def test_perimeter_correct(self):
         self.assertAlmostEqual(perimeter(9.4, 8), 34.8)
         self.assertAlmostEqual(perimeter(10, 12), 44)
 
-    def test_area_value_error(self):
-        self.assertRaises(ValueError, perimeter(-6, 84))
+    def test_perimeter_value_error(self):
+        self.assertRaises(ValueError, perimeter, -6, 84)
 
     def test_area_type_error(self):
-        self.assertRaises(TypeError, perimeter("str"))
+        self.assertRaises(TypeError, perimeter, -6, "str")
 
 
 if __name__ == '__main__':
