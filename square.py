d34c296ec4d803046ba79c4f43d71c8ac2bbb488
dev1lfreak <kvachuk.sergey@yandex.ru>
2023-12-21 09:04:45 +0300

    test : updated tests

--- square.py
+++ square.py
@@ -1,4 +1,10 @@
 import unittest
+
+"""
+    Проведение юнит-тестов для модуля circle.py, 
+    а именно методов area и perimeter
+"""
+
 def area(a):
     return a * a
 
@@ -6,27 +12,40 @@ def area(a):
 def perimeter(a):
     return 4 * a
 
+
 class SquareTestCase(unittest.TestCase):
+    def test_area_zero(self):
+        res = area(0)
+        self.assertEqual(res, 0)
 
-    def test_zero(self):
-        res_p = perimeter(0)
-        res_a = area(0)
+    def test_area_multiple(self):
+        res1 = area(5)
+        res2 = area(10)
+        self.assertEqual(res1, 25)
+        self.assertEqual(res2, 100)
 
-        self.assertEqual(res_p, 0)
-        self.assertEqual(res_a, 0)
+    def test_area_float_number(self):
+        res = area(4.5)
+        self.assertAlmostEqual(res, 20.25, places=2)
 
-    def test_work_perimeter(self):
-        res = perimeter(10)
-        self.assertEqual(res, 40)
+    def test_area_negative_number(self):
+        with self.assertRaises(Exception):
+            area(-10)
 
-    def test_work_square(self):
-        res = area(10)
-        self.assertEqual(res, 100)
+    def test_perimetr_multiple(self):
+        res1 = perimeter(4)
+        res2 = perimeter(10)
+        self.assertEqual(res1, 16)
+        self.assertEqual(res2, 40)
 
-    def test_negative_perimeter(self):
-        res = perimeter(-15)
-        self.assertEqual(res, -1)
+    def test_perimetr_float_number(self):
+        res = perimeter(3.6)
+        self.assertAlmostEqual(res, 14.4, places=1)
 
-    def test_negative_square(self):
-        res = area(-15)
-        self.assertEqual(res, -1)
+    def test_perimetr_negative_number(self):
+        with self.assertRaises(Exception):
+            perimeter(-20)
+
+    def test_perimetr_zero(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
--- triangle.py
+++ triangle.py
@@ -1,43 +1,42 @@
 import unittest
 
-
 def area(a, h):
     return a * h / 2
 
-
-def perimeter(a, b, c):
+def perimetr(a, b, c):
     return a + b + c
 
-
 class TriangleTestCase(unittest.TestCase):
+    def test_area_zero(self):
+        res = area(0, 0)
+        self.assertEqual(res, 0)
 
-    def test_zero(self):
-        res_p = perimeter(0, 0, 0)
-        res_a = area(0, 0)
+    def test_area_multiple(self):
+        res1 = area(4, 3)
+        res2 = area(11, 3)
+        self.assertAlmostEqual(res1, 6.0, places=1)
+        self.assertAlmostEqual(res2, 16.5, places=1)
 
-        self.assertEqual(res_p, 0)
-        self.assertEqual(res_a, 0)
+    def test_area_float_number(self):
+        res = area(10.6, 2)
+        self.assertAlmostEqual(res, 10.6, places=1)
 
-    def test_work_perimeter(self):
-        res = perimeter(15, 20, 10)
-        self.assertEqual(res, 45)
+    def test_area_negative_number(self):
+        with self.assertRaises(Exception):
+            area(-10, -20)
 
-    def test_work_square(self):
-        res = area(20, 5)
-        self.assertEqual(res, 50)
+    def test_perimetr_zero(self):
+        res = perimetr(0, 0, 0)
+        self.assertEqual(res, 0)
 
-    def test_with_zero_perimeter(self):
-        res = perimeter(0, 15, 10)
-        self.assertEqual(res, -1)
+    def test_perimetr_multiple(self):
+        res1 = perimetr(5, 3, 1)
+        self.assertEqual(res1, 9)
 
-    def test_with_zero_square(self):
-        res = area(0, 15)
-        self.assertEqual(res, -1)
+    def test_perimetr_float_number(self):
+        res = perimetr(10.5, 2, 3)
+        self.assertEqual(res, 15.5)
 
-    def test_negative_perimeter(self):
-        res = perimeter(-10, -15, 8)
-        self.assertEqual(res, -1)
-
-    def test_negative_square(self):
-        res = area(-10, -15)
-        self.assertEqual(res, -1)
+    def test_perimetr_negative_number(self):
+        with self.assertRaises(Exception):
+            perimetr(-10, -10, -10)
