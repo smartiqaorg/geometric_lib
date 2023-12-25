02ab9bf35f44b20286551f55f4708eb8c32b3ea3
Victoria Kotelevskaya <viktoriakotelevskaya39@gmail.com>
2023-12-25 10:18:40 +0300

    Change test_circle.py

--- test_circle.py
+++ test_circle.py
@@ -2,28 +2,27 @@ import unittest
 import math
 from circle import area
 from circle import perimeter
-class SquareTestCase(unittest.TestCase):
+
+class CircleTestCase(unittest.TestCase):
     def test_1_area(self):
         res = area(5)
-        self.assertEqual(res, math.pi * 25)
+        self.assertAlmostEqual(res, round(78.53975, 3))
 
     def test_2_area(self):
         res = area(13)
-        self.assertEqual(res, math.pi * 169)
+        self.assertAlmostEqual(res, round(530.92871, 3))
         
     def test_3_area(self):
         with self.assertRaises(ValueError) as context:
             area(-13)
-    
-
 
     def test_1_perimeter(self):
         res = perimeter(3)
-        self.assertEqual(res, math.pi * 6)
+        self.assertAlmostEqual(res, round(18.84954, 3))
 
     def test_2_perimeter(self):
         res = perimeter(30)
-        self.assertEqual(res, math.pi * 60)
+        self.assertAlmostEqual(res, round(188.4954, 3))
 
 if __name__ == '__main__':
     unittest.main()
