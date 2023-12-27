12d23246e0a857f5610b870aa086bf5d85471f09
ssandaa <144237596+ssandaa@users.noreply.github.com>
2023-12-27 22:22:13 +0300

    test: Update test_rectangle.py

--- test_rectangle.py
+++ test_rectangle.py
@@ -6,12 +6,12 @@ class RectangleTestCase(unittest.TestCase):
         self.assertEqual(res, 20)
 
     def test_rectangle_area_neNorm(self):
-        with self.assertEqual(ValueError):
+        with self.assertRaises(ValueError):
             area(4, -5)
 
     def test_rectangle_area_real(self):
         res = area(4.5, 5.5)
-        self.assertEqual(res, 24.75)
+        self.assertAlmostEqual(res, 24.75)
 
     def test_rectangle_area_zero(self):
         res = area(4, 0)
@@ -22,12 +22,12 @@ class RectangleTestCase(unittest.TestCase):
         self.assertEqual(res, 18)
 
     def test_rectangle_perimeter_neNorm(self):
-        with self.assertEqual(rValueError):
+        with self.assertRaises(ValueError):
             perimeter(4, -5)
 
     def test_rectangle_perimeter_real(self):
         res = perimeter(4.5, 5.5)
-        self.assertEqual(res, 20.0)
+        self.assertAlmostEqual(res, 20.0)
 
     def test_rectangle_perimeter_zero(self):
         res =  perimeter(4, 0)
