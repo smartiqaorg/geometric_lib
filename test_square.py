00f239601d28dcaf5be797f12e1f6bc930b2a31d
ssandaa <144237596+ssandaa@users.noreply.github.com>
2023-12-27 22:43:11 +0300

    test: Update test_square.py

--- test_square.py
+++ test_square.py
@@ -1,37 +1,38 @@
 import unittest
-from square import area, perimeter
+from circle import area,perimeter
 
-class SquareTestCase(unittest.TestCase):
-    def test_square_area_norm(self):
-        res = area(4)
-        self.assertEqual(res, 16)
+class CircleTestCase(unittest.TestCase):
+    def test_circle_area_norm(self):
+        res = area(5)
+        self.assertAlmostEqual(res, 78.540,delta = 0.1)
 
-    def test_square_area_neNorm(self):
+    def test_circle_area_neNorm(self):
         with self.assertRaises(ValueError):
-            area(-4)
+            area(-6)
+        
+    def test_circle_area_real(self):
+        res = area(5.5)
+        self.assertAlmostEqual(res, 95.033,delta = 0.1)
 
-    def test_square_area_real(self):
-        res = area(4.5)
-        self.assertAlmostEqual(res, 20.25)
-
-    def test_square_area_zero(self):
-        res = area(0)
+    def test_circle_area_zero(self):
+        res = area(0) 
         self.assertEqual(res, 0)
 
-    def test_square_perimeter_norm(self):
-        res = perimeter(4)
-        self.assertEqual(res, 16)
+    def test_circle_perimeter_norm(self):
+        res = perimeter(5)
+        self.assertAlmostEqual(res, 31.416, delta = 0.1)
 
-    def test_square_perimeter_neNorm(self):
+    def test_circle_perimeter_neNorm(self):
         with self.assertRaises(ValueError):
-            perimeter(-4)
+            perimeter(-5)
 
-    def test_square_perimeter_real(self):
-        res = perimeter(4.5)
-        self.assertAlmostEqual(res, 18.0)
+    def test_circle_perimeter_real(self):
+        res = perimeter(5.5)
+        self.assertAlmostEqual(res, 34.558, delta = 0.1)
 
-    def test_square_perimeter_zero(self):
+    def test_circle_perimeter_zero(self):
         res = perimeter(0)
         self.assertEqual(res, 0)
+
 if __name__ == '__main__':
     unittest.main()
