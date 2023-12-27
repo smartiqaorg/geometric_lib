7dd80092bcd3d057904461a660e163e35a8f8bf6
ssandaa <144237596+ssandaa@users.noreply.github.com>
2023-12-27 22:38:12 +0300

    test: Update test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -6,12 +6,12 @@ class TriangleTestCase(unittest.TestCase):
         self.assertEqual(res, 10.0)
 
     def test_triangle_area_neNorm(self):
-        with self.assertEqual(ValueError):
+        with self.assertRaises(ValueError):
             area(4, -5)
 
     def test_triangle_area_real(self):
         res = area(4.5, 5.5)
-        self.assertEqual(res, 12.375)
+        self.assertAlmostEqual(res, 12.375)
 
     def test_triangle_area_zero(self):
         res = area(4, 0)
@@ -22,16 +22,16 @@ class TriangleTestCase(unittest.TestCase):
         self.assertEqual(res, 15)
 
     def test_triangle_perimeter_neNorm(self):
-        with self.assertEqual(ValueError):
+        with self.assertRaises(ValueError):
             perimeter(4, 5, -6)
 
     def test_triangle_perimeter_real(self):
         res = perimeter(4.5, 5.5, 6.5)
-        self.assertEqual(res, 16.5)
+        self.assertAlmostEqual(res, 16.5)
 
     def test_triangle_perimeter_zero(self):
         res = perimeter(4, 0, 4.5)
-        self.assertEqual(res, 8.5)
+        self.assertAlmostEqual(res, 8.5)
 
 
 if __name__ == '__main__':
