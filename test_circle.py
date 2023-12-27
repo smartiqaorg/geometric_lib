72a3dad117bec3d037e9289c6d013068af2ff4bc
ssandaa <144237596+ssandaa@users.noreply.github.com>
2023-12-27 22:41:02 +0300

    test: Update test_circle.py

--- test_circle.py
+++ test_circle.py
@@ -4,26 +4,26 @@ from circle import area,perimeter
 class CircleTestCase(unittest.TestCase):
     def test_circle_area_norm(self):
         res = area(5)
-        self.asserAlmosttEqual(res, 78.540,delta = 0.1)
+        self.assertAlmostEqual(res, 78.540,delta = 0.1)
 
     def test_circle_area_neNorm(self):
         with self.assertRaises(ValueError):
-            area(-5)
+            area(-6)
         
     def test_circle_area_real(self):
         res = area(5.5)
-        self.assertAlmostEqual(res, 95.033, delta = 0.1)
+        self.assertAlmostEqual(res, 95.033,delta = 0.1)
 
     def test_circle_area_zero(self):
-        res = area(0)
+        res = area(0) 
         self.assertEqual(res, 0)
 
     def test_circle_perimeter_norm(self):
         res = perimeter(5)
-        self.assertEqual(res, 31.416, delta = 0.1)
+        self.assertAlmostEqual(res, 31.416, delta = 0.1)
 
     def test_circle_perimeter_neNorm(self):
-        with self.assertEqual(ValueError):
+        with self.assertRaises(ValueError):
             perimeter(-5)
 
     def test_circle_perimeter_real(self):
